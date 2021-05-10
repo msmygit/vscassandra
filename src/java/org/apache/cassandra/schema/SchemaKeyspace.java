ef77194cbf7f125c1ea78e2258c7a75829e97185
Jaroslaw Grabowski <jaroslaw.grabowski@datastax.com>
2021-05-10 17:07:24 +0200

    STAR-530 SchemaKeyspace is compatible with riptano/6.8-cndb (#133)
    * STAR-530 SchemaKeyspace is compatible with riptano/6.8-cndb
    
    Add missing columns in SchemaKeyspace, which results in sstable
    compatibility with riptano/6.8-cndb (used by CNDB). This makes
    full-stop upgrade from Astra serverless to dse-db possible.
    
    Additionally fix reading commit log coming from riptano/6.8-cndb.
    Commit log version 680 is read with MessagingService.VERSION_40
    messaging version but in truth it doesn't matter as the version
    is never used in deserialization.
    
    * STAR-530 add UCS mock
    
    Add temp UCS mock to support full-stop upgrades from CNDB to
    dse-db. The mock should be removed by UCS feature PR.
    
    (cherry picked from commit 03ad8bab34840501cacf1bcee2028ab2a62cbcdf)
    (cherry picked from commit a4598b5346c9fbf3f46a1ffe96e004df412f3310)
    (cherry picked from commit 2492b498d13076fd78bc96d4d09295e359b982bd)
    (cherry picked from commit 805ab0551420c4cbf0ef14e5f8c06a224f7c84aa)

--- src/java/org/apache/cassandra/schema/SchemaKeyspace.java
+++ src/java/org/apache/cassandra/schema/SchemaKeyspace.java
@@ -89,6 +89,7 @@ final class SchemaKeyspace
               + "keyspace_name text,"
               + "durable_writes boolean,"
               + "replication frozen<map<text, text>>,"
+              + "graph_engine text,"
               + "PRIMARY KEY ((keyspace_name)))");
 
     private static final TableMetadata Tables =
@@ -113,6 +114,7 @@ final class SchemaKeyspace
               + "max_index_interval int,"
               + "memtable_flush_period_in_ms int,"
               + "min_index_interval int,"
+              + "nodesync frozen<map<text, text>>,"
               + "read_repair_chance double," // no longer used, left for drivers' sake
               + "speculative_retry text,"
               + "additional_write_policy text,"
@@ -132,6 +134,7 @@ final class SchemaKeyspace
               + "kind text,"
               + "position int,"
               + "type text,"
+              + "required_for_liveness boolean,"
               + "PRIMARY KEY ((keyspace_name), table_name, column_name))");
 
     private static final TableMetadata DroppedColumns =
@@ -181,10 +184,12 @@ final class SchemaKeyspace
               + "max_index_interval int,"
               + "memtable_flush_period_in_ms int,"
               + "min_index_interval int,"
+              + "nodesync frozen<map<text, text>>,"
               + "read_repair_chance double," // no longer used, left for drivers' sake
               + "speculative_retry text,"
               + "additional_write_policy text,"
               + "cdc boolean,"
+              + "version int,"
               + "read_repair text,"
               + "PRIMARY KEY ((keyspace_name), view_name))");
 
@@ -221,6 +226,9 @@ final class SchemaKeyspace
               + "language text,"
               + "return_type text,"
               + "called_on_null_input boolean,"
+              + "deterministic boolean,"
+              + "monotonic boolean,"
+              + "monotonic_on frozen<list<text>>,"
               + "PRIMARY KEY ((keyspace_name), function_name, argument_types))");
 
     private static final TableMetadata Aggregates =
@@ -235,6 +243,7 @@ final class SchemaKeyspace
               + "return_type text,"
               + "state_func text,"
               + "state_type text,"
+              + "deterministic boolean,"
               + "PRIMARY KEY ((keyspace_name), aggregate_name, argument_types))");
 
     private static final List<TableMetadata> ALL_TABLE_METADATA =
