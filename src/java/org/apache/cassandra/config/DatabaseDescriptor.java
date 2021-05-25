74ca734fbcbd60e5eefd0c99deaad419ef701668
Matt Fleming <matt@codeblueprint.co.uk>
2021-05-25 21:23:04 +0100

    STAR-674: Bump memtable_flush_writers to 8 same as DSE

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -662,7 +662,7 @@ public class DatabaseDescriptor
 
         if (conf.memtable_flush_writers == 0)
         {
-            conf.memtable_flush_writers = conf.data_file_directories.length == 1 ? 2 : 1;
+            conf.memtable_flush_writers = 8;
         }
 
         if (conf.memtable_flush_writers < 1)
