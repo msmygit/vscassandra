6f0f9a8b427b9363e385906b038a814a986e46f4
Piotr Kołaczkowski <pkolaczk@datastax.com>
2021-07-16 18:04:08 +0200

    STAR-821 Allow empty container elements
    Looks like the CqlParser can construct empty container elements
    when given invalid queries, before returning the error to the
    user.

--- src/java/org/apache/cassandra/cql3/WhereClause.java
+++ src/java/org/apache/cassandra/cql3/WhereClause.java
@@ -462,7 +462,6 @@ public final class WhereClause
 
         protected ContainerElement(Collection<ExpressionElement> children)
         {
-            assert children.size() >= 1: "ContainerElement cannot have 0 children";
             this.children = new ArrayList<>(children.size());
             this.children.addAll(children);
         }
@@ -544,10 +543,12 @@ public final class WhereClause
         @Override
         public String toString()
         {
-            return children
-                    .stream()
-                    .map(c -> children.size() > 1 && c.isCompound() ? '(' + c.toString() + ')': c.toString())
-                    .collect(Collectors.joining(operator().joinValue()));
+            return children.isEmpty()
+                    ? "()"
+                    : children
+                        .stream()
+                        .map(c -> children.size() > 1 && c.isCompound() ? '(' + c.toString() + ')':c.toString())
+                        .collect(Collectors.joining(operator().joinValue()));
         }
     }
 
