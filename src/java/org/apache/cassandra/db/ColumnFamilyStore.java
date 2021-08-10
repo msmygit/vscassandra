81450a61c6783263363d98b103f5e6b0c288cd84
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2021-08-10 14:01:25 +0200

    STAR-823: Fix deadlock in CompactionManager (#230)
    STAR-823: Refactor background compactions
    
    CompactionManager.BackgroundCompactionCandidate task is scheduled on
    compaction executor and when it detects there are compaction tasks
    to run, it starts each compaction task as a separate job on the same
    compaction executor and blocks until all tasks are finished.
    
    When the pool size for the executor is n, and there are n background
    tasks submitted in parallel, and all of them find that there are some
    compactions to run, they will schedule them and block until the tasks
    are finished. Though, the tasks cannot start because the pool is full
    - we have n background tasks there waiting for the compaction tasks
    that cannot start.
    
    Another thing (perhaps minor) is that we use getActiveCount() on
    the executor to check how many tasks it is currently running,
    and based on that information either schedule new tasks or not.
    The problem with this method is that it returns an approximate
    result and should not be used for making such decisions.
    
    To address those problems, running of background compactions was
    refactored. The whole logic for background compactions was extracted
    into a distinct class BackgroundCompactionsRunner. It allows flagging
    CFSs for compaction and schedules scans through the flagged CFSs
    on a dedicated executor so that scans and compaction tasks are no
    longer sharing the same executor.
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    (cherry picked from commit 96bf61c4cd80ee837ae34fe9c3076e5df48ed7fb)
    (cherry picked from commit 804b885f893c6e91727694c6d353c0ba8365e2cf)

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -50,6 +50,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.management.MalformedObjectNameException;
@@ -71,6 +72,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import com.google.common.primitives.Longs;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListenableFutureTask;
 import com.google.common.util.concurrent.RateLimiter;
@@ -381,11 +383,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
 
     public static Runnable getBackgroundCompactionTaskSubmitter()
     {
-        return () -> {
-            for (Keyspace keyspace : Keyspace.all())
-                for (ColumnFamilyStore cfs : keyspace.getColumnFamilyStores())
-                    CompactionManager.instance.submitBackground(cfs);
-        };
+        return () -> CompactionManager.instance.submitBackground(ImmutableSet.copyOf(all()));
     }
 
     @VisibleForTesting
@@ -2656,10 +2654,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     @Override
     public String toString()
     {
-        return "CFS(" +
-               "Keyspace='" + keyspace.getName() + '\'' +
-               ", ColumnFamily='" + name + '\'' +
-               ')';
+        return String.format("%s.%s", getKeyspaceName(), getTableName());
     }
 
     public void disableAutoCompaction()
@@ -2695,6 +2690,18 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean, Memtable.Owner
     public boolean isAutoCompactionDisabled()
     {
         return !this.strategyContainer.isEnabled();
+    }
+
+    public List<SSTableReader> getCandidatesForUpgrade()
+    {
+        Set<SSTableReader> compacting = getTracker().getCompacting();
+        return getLiveSSTables().stream()
+                                .filter(s -> !compacting.contains(s) && !s.descriptor.version.isLatestVersion())
+                                .sorted((o1, o2) -> {
+                                    File f1 = new File(o1.descriptor.filenameFor(Component.DATA));
+                                    File f2 = new File(o2.descriptor.filenameFor(Component.DATA));
+                                    return Longs.compare(f1.lastModified(), f2.lastModified());
+                                }).collect(Collectors.toList());
     }
 
     public SortedLocalRanges getLocalRanges()
