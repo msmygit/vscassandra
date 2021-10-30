69663ca67741ed356cf704c7d4c16d1d9ca58349
Jason Rutherglen <jasonrutherglen@Jasons-MacBook-Pro.local>
2021-10-29 21:21:39 -0600

    SAILuceneQueryTest matches all rows; compares sai and lucene AND queries

--- test/microbench/org/apache/cassandra/test/microbench/index/sai/v1/SAILuceneQueryTest.java
+++ test/microbench/org/apache/cassandra/test/microbench/index/sai/v1/SAILuceneQueryTest.java
@@ -27,9 +27,11 @@ import java.util.concurrent.TimeUnit;
 
 import io.netty.util.internal.ThreadLocalRandom;
 import org.apache.cassandra.cql3.CQLTester;
+import org.apache.cassandra.cql3.UntypedResultSet;
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.commitlog.CommitLog;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -42,8 +44,13 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TopFieldCollector;
+import org.apache.lucene.search.TopFieldDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -63,15 +70,17 @@ import org.openjdk.jmh.annotations.Warmup;
 
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 1, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 2, time = 2, timeUnit = TimeUnit.SECONDS)
+//@Warmup(iterations = 100000, time = 1, timeUnit = TimeUnit.SECONDS)
+//@Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 2, time = 5, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 30, timeUnit = TimeUnit.SECONDS)
 @Fork(value = 1)
 @Threads(1)
 @State(Scope.Benchmark)
 public class SAILuceneQueryTest extends CQLTester
 {
-    private static final int NUM_INVOCATIONS = 10_000;
-    public static int ROWS = 1_000_000;
+    private static final int NUM_INVOCATIONS = 100;
+    public static int ROWS = 100_000;
 
     static String keyspace;
     String table;
@@ -81,13 +90,17 @@ public class SAILuceneQueryTest extends CQLTester
     FSDirectory directory;
     DirectoryReader luceneReader;
     IndexSearcher indexSearcher;
+    int luceneQueryCount = 0;
+    int luceneQueriesWithResults = 0;
+    int saiQueriesWithResults = 0;
+    int saiQueryCount = 0;
 
     @Setup(Level.Trial)
     public void setup() throws Throwable
     {
         CQLTester.setUpClass();
         keyspace = createKeyspace("CREATE KEYSPACE %s with replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 } and durable_writes = false");
-        table = createTable(keyspace, "CREATE TABLE %s (key bigint, columnA bigint, columnB bigint, PRIMARY KEY(key))");
+        table = createTable(keyspace, "CREATE TABLE %s (key int, columnA int, columnB int, PRIMARY KEY(key))");
         createIndex("CREATE CUSTOM INDEX ON "+keyspace+"."+table+"(columnA) USING 'StorageAttachedIndex'");
         createIndex("CREATE CUSTOM INDEX ON "+keyspace+"."+table+"(columnB) USING 'StorageAttachedIndex'");
 
@@ -100,23 +113,25 @@ public class SAILuceneQueryTest extends CQLTester
 
         Path luceneDir = Files.createTempDirectory("jmh_lucene_test");
         directory = FSDirectory.open(luceneDir);
-        IndexWriterConfig config = new IndexWriterConfig(new WhitespaceAnalyzer());
+        IndexWriterConfig config = new IndexWriterConfig(new KeywordAnalyzer());
         IndexWriter indexWriter = new IndexWriter(directory, config);
 
         Document document = new Document();
 
         System.err.println("Writing "+ROWS+" rows");
-        for (long i = 0; i < ROWS; i++)
+        for (int i = 0; i < ROWS; i++)
         {
-            int columnAValue = ThreadLocalRandom.current().nextInt(0, ROWS / 50);
-            int columnBValue = ThreadLocalRandom.current().nextInt(0, ROWS / 1000);
+            int columnAValue = genColumnA();
+            int columnBValue = genColumnB();
 
-            execute(writeStatement, i, new Long(columnAValue), new Long(columnBValue));
+            execute(writeStatement, i, columnAValue, columnBValue);
 
-            StringField columnA = new StringField("columnA", Integer.toString(columnAValue), Field.Store.NO);
-            StringField columnB = new StringField("columnB", Integer.toString(columnBValue), Field.Store.NO);
+            StringField key = new StringField("key", Integer.toString(i), Field.Store.YES);
+            StringField columnA = new StringField("columnA", Integer.toString(columnAValue), Field.Store.YES);
+            StringField columnB = new StringField("columnB", Integer.toString(columnBValue), Field.Store.YES);
 
             document.clear();
+            document.add(key);
             document.add(columnA);
             document.add(columnB);
 
@@ -130,42 +145,90 @@ public class SAILuceneQueryTest extends CQLTester
         compact(keyspace);
 
         luceneReader = DirectoryReader.open(directory);
+
+        if (luceneReader.maxDoc() != ROWS)
+        {
+            throw new IllegalStateException("luceneReader.maxDoc="+luceneReader.maxDoc()+" rows="+ROWS);
+        }
+
         indexSearcher = new IndexSearcher(luceneReader);
+        indexSearcher.setQueryCache(null); // disable the default query cache
     }
 
     @TearDown(Level.Trial)
     public void teardown() throws IOException, ExecutionException, InterruptedException
     {
+        System.out.println("luceneQueriesWithResults="+luceneQueriesWithResults+" luceneQueryCount="+luceneQueryCount);
+        System.out.println("saiQueriesWithResults="+saiQueriesWithResults+" saiQueryCount="+saiQueryCount);
+
         luceneReader.close();
 
         CommitLog.instance.shutdownBlocking();
         CQLTester.cleanup();
     }
 
-    @Benchmark
-    @OperationsPerInvocation(NUM_INVOCATIONS)
-    @BenchmarkMode({ Mode.Throughput, Mode.AverageTime })
-    public Object readLucene() throws Throwable
+    public int genColumnA()
     {
-        int columnA = ThreadLocalRandom.current().nextInt(0, ROWS / 1000);
-        int columnB = ThreadLocalRandom.current().nextInt(0, ROWS / 10_000);
+        return ThreadLocalRandom.current().nextInt(0, 50);
+    }
+
+    public int genColumnB()
+    {
+        return ThreadLocalRandom.current().nextInt(0, 250);
+    }
+
+    @Benchmark
+    @OperationsPerInvocation(1)
+    @BenchmarkMode({ Mode.Throughput, Mode.AverageTime })
+    public Object queryExact2ANDLucene() throws Throwable
+    {
+        int columnA = genColumnA();
+        int columnB = genColumnB();
 
         BooleanQuery.Builder builder = new BooleanQuery.Builder();
         builder.add(new BooleanClause(new TermQuery(new Term("columnA", new BytesRef(Integer.toString(columnA)))), BooleanClause.Occur.MUST));
         builder.add(new BooleanClause(new TermQuery(new Term("columnB", new BytesRef(Integer.toString(columnB)))), BooleanClause.Occur.MUST));
-
         BooleanQuery query = builder.build();
 
-        return indexSearcher.search(query, 10);
+        luceneQueryCount++;
+
+        TopFieldCollector collector = TopFieldCollector.create(Sort.INDEXORDER, 10, null, false, false, false, false);
+
+        indexSearcher.search(query, collector);
+
+        TopFieldDocs topDocs = collector.topDocs();
+
+        if (topDocs.totalHits > 0)
+        {
+            luceneQueriesWithResults++;
+
+            // iterate on each result
+            for (ScoreDoc doc : topDocs.scoreDocs)
+            {
+                Document document = indexSearcher.doc(doc.doc);
+            }
+        }
+        return topDocs;
     }
 
     @Benchmark
-    @OperationsPerInvocation(NUM_INVOCATIONS)
+    @OperationsPerInvocation(1)
     @BenchmarkMode({ Mode.Throughput, Mode.AverageTime })
-    public Object readSAI() throws Throwable
+    public Object queryExact2ANDSAI() throws Throwable
     {
-        int columnA = ThreadLocalRandom.current().nextInt(0, ROWS / 1000);
-        int columnB = ThreadLocalRandom.current().nextInt(0, ROWS / 10_000);
-        return execute(readStatement, new Long(columnA), new Long(columnB));
+        saiQueryCount++;
+        int columnA = genColumnA();
+        int columnB = genColumnB();
+        UntypedResultSet results = execute(readStatement, columnA, columnB);
+        if (results.size() > 0)
+        {
+            // iterate on each result
+            for (UntypedResultSet.Row row : results)
+            {
+
+            }
+            saiQueriesWithResults++;
+        }
+        return results;
     }
 }
