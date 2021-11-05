e7ca94c40ddd904ca50bb1f734a815b6a7ec620e
dan jatnieks <jatnieks@pobox.com>
2021-11-05 13:50:07 -0700

    STAR-891 Add pluggable disk/commit log error handlers and shutdown hooks (#271)
    (cherry picked from commit c6cd4ee6673f9b43f7de7630e7669a873f4bf01c)
    (cherry picked from commit fb5c241b5bf22ae4683152b82a04605f925ef108)
    (cherry picked from commit f7f8e0a3adfcc09dcf365a6d452d9bbdbe6c76a9)
    (cherry picked from commit 0ff7339d9da4a64994c9d92ff89832463ffbcf55)

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -268,7 +268,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public volatile VersionedValue.VersionedValueFactory valueFactory = new VersionedValue.VersionedValueFactory(TokenMetadataProvider.instance.getTokenMetadata().partitioner);
 
-    private Thread drainOnShutdown = null;
     private volatile boolean isShutdown = false;
     private final List<Runnable> preShutdownHooks = new ArrayList<>();
     private final List<Runnable> postShutdownHooks = new ArrayList<>();
@@ -852,7 +851,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
 
         // daemon threads, like our executors', continue to run while shutdown hooks are invoked
-        drainOnShutdown = NamedThreadFactory.createThread(new WrappedRunnable()
+        Thread drainOnShutdown = NamedThreadFactory.createThread(new WrappedRunnable()
         {
             @Override
             public void runMayThrow() throws InterruptedException, ExecutionException, IOException
@@ -865,7 +864,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                 LoggingSupportFactory.getLoggingSupport().onShutdown();
             }
         }, "StorageServiceShutdownHook");
-        Runtime.getRuntime().addShutdownHook(drainOnShutdown);
+        JVMStabilityInspector.registerShutdownHook(drainOnShutdown, this::onShutdownHookRemoved);
 
         replacing = isReplacing();
 
@@ -961,11 +960,8 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     /**
      * In the event of forceful termination we need to remove the shutdown hook to prevent hanging (OOM for instance)
      */
-    public void removeShutdownHook()
+    public void onShutdownHookRemoved()
     {
-        if (drainOnShutdown != null)
-            Runtime.getRuntime().removeShutdownHook(drainOnShutdown);
-
         if (FBUtilities.isWindows)
             WindowsTimer.endTimerPeriod(DatabaseDescriptor.getWindowsTimerInterval());
     }
@@ -6049,15 +6045,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     public boolean getNativeTransportRateLimitingEnabled()
     {
         return DatabaseDescriptor.getNativeTransportRateLimitingEnabled();
-    }
-
-    @VisibleForTesting
-    public void shutdownServer()
-    {
-        if (drainOnShutdown != null)
-        {
-            Runtime.getRuntime().removeShutdownHook(drainOnShutdown);
-        }
     }
 
     @Override
