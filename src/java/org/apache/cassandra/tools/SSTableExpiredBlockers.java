apache/cassandra

CASSANDRA-17044: Refactor schema updates
The code which is responsible for dealing with SchemaKeyspace (storing changes in system tables) has been extracted out to DefaultSchemaUpdateHandler.
The other changes are the consequences of that change.

Delegate some functionality related to MigrationCoordinator through DefaultSchemaUpdateHandler
DefaultSchemaUpdateHandler also registers itself as a endpoint event subscriber, so we may avoid explicit calls on MigrationCoordinator when certain events happen.

Squash SchemaManager.loadFromDisk methods

Make some methods private in SchemaManager

Remove direct usages of MigrationCoordinator
After previous changes, it became possible to remove some methods and reduce visibility of others. The main change is that we no longer need to get direct MigrationCoordinator instance outside DefaultSchemaUpdateHandler.

src/java/org/apache/cassandra/tools/SSTableExpiredBlockers.java
@@ -58,7 +58,7 @@ public class SSTableExpiredBlockers
 
         String keyspace = args[args.length - 2];
         String columnfamily = args[args.length - 1];
-        SchemaManager.instance.loadFromDisk(false);
+        SchemaManager.instance.loadFromDisk();
 
         TableMetadata metadata = SchemaManager.instance.validateTable(keyspace, columnfamily);
 

src/java/org/apache/cassandra/tools/StandaloneVerifier.java
@@ -61,7 +61,7 @@ public class StandaloneVerifier
         try
         {
             // load keyspace descriptions.
-            SchemaManager.instance.loadFromDisk(false);
+            SchemaManager.instance.loadFromDisk();
 
             boolean hasFailed = false;
 

tools/stress/src/org/apache/cassandra/io/sstable/StressCQLSSTableWriter.java
@@ -32,6 +32,7 @@ import org.apache.cassandra.cql3.CQLStatement;
 import org.apache.cassandra.cql3.statements.schema.CreateTableStatement;
 import org.apache.cassandra.cql3.statements.schema.CreateTypeStatement;
 import org.apache.cassandra.schema.SchemaManager;
+import org.apache.cassandra.schema.SchemaTransformations;
 import org.apache.cassandra.schema.TableId;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.config.DatabaseDescriptor;
@@ -566,15 +567,14 @@ public class StressCQLSSTableWriter implements Closeable
             }
         }
 
-        private static void createTypes(String keyspace, List<CreateTypeStatement.Raw> typeStatements)
+        private static Types createTypes(String keyspace, List<CreateTypeStatement.Raw> typeStatements)
         {
             KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(keyspace);
             Types.RawBuilder builder = Types.rawBuilder(keyspace);
             for (CreateTypeStatement.Raw st : typeStatements)
                 st.addToRawBuilder(builder);
 
-            ksm = ksm.withSwapped(builder.build());
-            SchemaManager.instance.load(ksm);
+            return builder.build();
         }
 
         public static ColumnFamilyStore createOfflineTable(String schema, List<File> directoryList)
@@ -590,10 +590,10 @@ public class StressCQLSSTableWriter implements Closeable
         {
             String keyspace = schemaStatement.keyspace();
 
-            if (SchemaManager.instance.getKeyspaceMetadata(keyspace) == null)
-                SchemaManager.instance.load(KeyspaceMetadata.create(keyspace, KeyspaceParams.simple(1)));
+            SchemaManager.instance.transform(SchemaTransformations.addKeyspace(KeyspaceMetadata.create(keyspace, KeyspaceParams.simple(1)), true));
 
-            createTypes(keyspace, typeStatements);
+            Types types = createTypes(keyspace, typeStatements);
+            SchemaManager.instance.transform(SchemaTransformations.addTypes(types, true));
 
             KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(keyspace);
 
@@ -617,7 +617,7 @@ public class StressCQLSSTableWriter implements Closeable
             ColumnFamilyStore cfs =  ColumnFamilyStore.createColumnFamilyStore(ks, tableMetadata.name, TableMetadataRef.forOfflineTools(tableMetadata), directories, false, false, true);
 
             ks.initCfCustom(cfs);
-            SchemaManager.instance.load(ksm.withSwapped(ksm.tables.with(cfs.metadata())));
+            SchemaManager.instance.transform(SchemaTransformations.addTable(tableMetadata, true));
 
             return cfs;
         }

msmygit/vscassandra

src/java/org/apache/cassandra/tools/SSTableExpiredBlockers.java
@@ -60,7 +60,7 @@ public class SSTableExpiredBlockers
 
         String keyspace = args[args.length - 2];
         String columnfamily = args[args.length - 1];
-        Schema.instance.loadFromDisk(false);
+        Schema.instance.loadFromDisk();
 
         TableMetadata metadata = Schema.instance.validateTable(keyspace, columnfamily);
 

src/java/org/apache/cassandra/tools/StandaloneVerifier.java
@@ -62,7 +62,7 @@ public class StandaloneVerifier
         try
         {
             // load keyspace descriptions.
-            Schema.instance.loadFromDisk(false);
+            Schema.instance.loadFromDisk();
 
             boolean hasFailed = false;
 

tools/stress/src/org/apache/cassandra/io/sstable/StressCQLSSTableWriter.java
@@ -35,6 +35,7 @@ import org.apache.cassandra.schema.TableId;
 import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.schema.SchemaTransformations;
 import org.apache.cassandra.cql3.CQLFragmentParser;
 import org.apache.cassandra.cql3.ColumnSpecification;
 import org.apache.cassandra.cql3.CqlParser;
@@ -568,15 +569,14 @@ public class StressCQLSSTableWriter implements Closeable
             }
         }
 
-        private static void createTypes(String keyspace, List<CreateTypeStatement.Raw> typeStatements)
+        private static Types createTypes(String keyspace, List<CreateTypeStatement.Raw> typeStatements)
         {
             KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(keyspace);
             Types.RawBuilder builder = Types.rawBuilder(keyspace);
             for (CreateTypeStatement.Raw st : typeStatements)
                 st.addToRawBuilder(builder);
 
-            ksm = ksm.withSwapped(builder.build());
-            Schema.instance.load(ksm);
+            return builder.build();
         }
 
         public static ColumnFamilyStore createOfflineTable(String schema, List<File> directoryList)
@@ -592,10 +592,10 @@ public class StressCQLSSTableWriter implements Closeable
         {
             String keyspace = schemaStatement.keyspace();
 
-            if (Schema.instance.getKeyspaceMetadata(keyspace) == null)
-                Schema.instance.load(KeyspaceMetadata.create(keyspace, KeyspaceParams.simple(1)));
+            Schema.instance.transform(SchemaTransformations.addKeyspace(KeyspaceMetadata.create(keyspace, KeyspaceParams.simple(1)), true));
 
-            createTypes(keyspace, typeStatements);
+            Types types = createTypes(keyspace, typeStatements);
+            Schema.instance.transform(SchemaTransformations.addTypes(types, true));
 
             KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(keyspace);
 
@@ -619,7 +619,7 @@ public class StressCQLSSTableWriter implements Closeable
             ColumnFamilyStore cfs =  ColumnFamilyStore.createColumnFamilyStore(ks, tableMetadata.name, TableMetadataRef.forOfflineTools(tableMetadata), directories, false, false, true);
 
             ks.initCfCustom(cfs);
-            Schema.instance.load(ksm.withSwapped(ksm.tables.with(cfs.metadata())));
+            Schema.instance.transform(SchemaTransformations.addTable(tableMetadata, true));
 
             return cfs;
         }