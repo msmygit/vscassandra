apache/cassandra

CASSANDRA-17044: Extracted SharedSchema object and SchemaTransformationResult
Static subclass TransformationResult was removed from SchemaManager and added to SchemaTransformation class.

Also removed fields like success or exception as it made no sense for them to be in that class.

src/java/org/apache/cassandra/schema/MigrationCoordinator.java
@@ -261,8 +261,8 @@ public class MigrationCoordinator
             logger.debug("Not pulling schema for version {}, because schema versions match: " +
                          "local={}, remote={}",
                          version,
-                         SchemaManager.schemaVersionToString(SchemaManager.instance.getVersion()),
-                         SchemaManager.schemaVersionToString(version));
+                         SharedSchema.schemaVersionToString(SchemaManager.instance.getVersion()),
+                         SharedSchema.schemaVersionToString(version));
             return false;
         }
         return true;
@@ -324,8 +324,8 @@ public class MigrationCoordinator
             logger.debug("Immediately submitting migration task for {}, " +
                          "schema versions: local={}, remote={}",
                          endpoint,
-                         SchemaManager.schemaVersionToString(SchemaManager.instance.getVersion()),
-                         SchemaManager.schemaVersionToString(version));
+                         SharedSchema.schemaVersionToString(SchemaManager.instance.getVersion()),
+                         SharedSchema.schemaVersionToString(version));
             return true;
         }
         return false;

msmygit/vscassandra

src/java/org/apache/cassandra/schema/MigrationCoordinator.java
@@ -265,8 +265,8 @@ public class MigrationCoordinator
             logger.debug("Not pulling schema for version {}, because schema versions match: " +
                          "local={}, remote={}",
                          version,
-                         Schema.schemaVersionToString(Schema.instance.getVersion()),
-                         Schema.schemaVersionToString(version));
+                         SharedSchema.schemaVersionToString(Schema.instance.getVersion()),
+                         SharedSchema.schemaVersionToString(version));
             return false;
         }
         return true;
@@ -328,8 +328,8 @@ public class MigrationCoordinator
             logger.debug("Immediately submitting migration task for {}, " +
                          "schema versions: local={}, remote={}",
                          endpoint,
-                         Schema.schemaVersionToString(Schema.instance.getVersion()),
-                         Schema.schemaVersionToString(version));
+                         SharedSchema.schemaVersionToString(Schema.instance.getVersion()),
+                         SharedSchema.schemaVersionToString(version));
             return true;
         }
         return false;