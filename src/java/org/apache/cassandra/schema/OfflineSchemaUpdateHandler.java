apache/cassandra

CASSANDRA-17044: Add SchemaUpdateHandlerFactoryProvider
Factory is used to create the right instance depending on the context - whether we are online or not (server mode / tool or client mode). Provider is for creating the factory so that we can switch the implementation by providing a system property. This can be also useful for testing.

src/java/org/apache/cassandra/schema/OfflineSchemaUpdateHandler.java
@@ -59,15 +59,10 @@ public class OfflineSchemaUpdateHandler implements SchemaUpdateHandler
         return true;
     }
 
-    private SharedSchema schema()
-    {
-        return schema;
-    }
-
     @Override
     public synchronized SchemaTransformationResult apply(SchemaTransformation transformation, boolean local)
     {
-        SharedSchema before = schema();
+        SharedSchema before = schema;
         Keyspaces afterKeyspaces = transformation.apply(before.getKeyspaces());
         Keyspaces.KeyspacesDiff diff = Keyspaces.diff(before.getKeyspaces(), afterKeyspaces);
 

msmygit/vscassandra

src/java/org/apache/cassandra/schema/OfflineSchemaUpdateHandler.java
@@ -59,15 +59,10 @@ public class OfflineSchemaUpdateHandler implements SchemaUpdateHandler
         return true;
     }
 
-    private DistributedSchema schema()
-    {
-        return schema;
-    }
-
     @Override
     public synchronized SchemaTransformationResult apply(SchemaTransformation transformation, boolean local)
     {
-        DistributedSchema before = schema();
+        DistributedSchema before = schema;
         Keyspaces afterKeyspaces = transformation.apply(before.getKeyspaces());
         Keyspaces.KeyspacesDiff diff = Keyspaces.diff(before.getKeyspaces(), afterKeyspaces);
 