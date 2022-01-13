3a6218fc709ac4d30ac64a6b96121e70915e88a5
Zhao Yang <zhaoyangsingapore@gmail.com>
2022-01-13 15:51:10 +0800

    STAR-992: add APIs for replayer to discard flushed remote sstables (#310)
    (cherry picked from commit 343a71733bcf39df209e4008c6ffb8b614567542)

--- test/distributed/org/apache/cassandra/distributed/impl/Instance.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -515,7 +515,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
                 // Replay any CommitLogSegments found on disk
                 try
                 {
-                    CommitLog.instance.recoverSegmentsOnDisk();
+                    CommitLog.instance.recoverSegmentsOnDisk(ColumnFamilyStore.FlushReason.STARTUP);
                 }
                 catch (IOException e)
                 {
