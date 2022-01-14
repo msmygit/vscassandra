2eeb071b0c1543519e2d6ec8c63270282d3b5b09
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2022-01-14 12:40:07 +0100

    STAR-893: apply some performance improvements to Nodes impl (#292)
    * STAR-893: Use read-only interface instead of preventive duplication
    
    * STAR-893: Simplify access to commonly used fields
    
    (cherry picked from commit be31ce7d8050c140b48c4d573f48d4e742a9c40a)

--- src/java/org/apache/cassandra/db/SystemKeyspace.java
+++ src/java/org/apache/cassandra/db/SystemKeyspace.java
@@ -37,8 +37,6 @@ import com.google.common.collect.Sets;
 import com.google.common.io.ByteStreams;
 import com.google.common.util.concurrent.ListenableFuture;
 import org.apache.commons.lang3.ObjectUtils;
-
-import org.apache.cassandra.io.util.File;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -59,9 +57,9 @@ import org.apache.cassandra.io.util.*;
 import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.metrics.RestorableMeter;
 import org.apache.cassandra.net.MessagingService;
-import org.apache.cassandra.nodes.NodeInfo;
+import org.apache.cassandra.nodes.INodeInfo;
+import org.apache.cassandra.nodes.IPeerInfo;
 import org.apache.cassandra.nodes.Nodes;
-import org.apache.cassandra.nodes.PeerInfo;
 import org.apache.cassandra.nodes.TruncationRecord;
 import org.apache.cassandra.schema.*;
 import org.apache.cassandra.schema.SchemaConstants;
@@ -730,7 +728,7 @@ public final class SystemKeyspace
     public static SetMultimap<InetAddressAndPort, Token> loadTokens()
     {
         SetMultimap<InetAddressAndPort, Token> tokenMap = HashMultimap.create();
-        Nodes.peers().get().filter(PeerInfo::isExisting).forEach(info -> tokenMap.putAll(info.getPeerAddressAndPort(), info.getTokens()));
+        Nodes.peers().get().filter(IPeerInfo::isExisting).forEach(info -> tokenMap.putAll(info.getPeerAddressAndPort(), info.getTokens()));
         return tokenMap;
     }
 
@@ -740,7 +738,7 @@ public final class SystemKeyspace
      */
     public static Map<InetAddressAndPort, UUID> loadHostIds()
     {
-        return Nodes.peers().get().filter(PeerInfo::isExisting).collect(Collectors.toMap(PeerInfo::getPeerAddressAndPort, NodeInfo::getHostId));
+        return Nodes.peers().get().filter(IPeerInfo::isExisting).collect(Collectors.toMap(IPeerInfo::getPeerAddressAndPort, INodeInfo::getHostId));
     }
 
     /**
@@ -751,7 +749,7 @@ public final class SystemKeyspace
      */
     public static InetAddressAndPort getPreferredIP(InetAddressAndPort ep)
     {
-        PeerInfo info = Nodes.peers().get(ep);
+        IPeerInfo info = Nodes.peers().get(ep);
         if (info != null && info.getPreferredAddressAndPort() != null && info.isExisting())
             return info.getPreferredAddressAndPort();
         else
@@ -766,7 +764,7 @@ public final class SystemKeyspace
         return Nodes.peers()
                     .get()
                     .filter(p -> p.getDataCenter() != null && p.getRack() != null && p.isExisting())
-                    .collect(Collectors.toMap(PeerInfo::getPeerAddressAndPort, p -> {
+                    .collect(Collectors.toMap(IPeerInfo::getPeerAddressAndPort, p -> {
                         Map<String, String> dcRack = new HashMap<>();
                         dcRack.put("data_center", p.getDataCenter());
                         dcRack.put("rack", p.getRack());
@@ -786,7 +784,10 @@ public final class SystemKeyspace
         if (FBUtilities.getBroadcastAddressAndPort().equals(ep))
             return CURRENT_VERSION;
         else
-            return Nodes.peers().getOpt(ep).filter(PeerInfo::isExisting).map(NodeInfo::getReleaseVersion).orElse(null);
+        {
+            IPeerInfo peer = Nodes.peers().get(ep);
+            return peer != null && peer.isExisting() ? peer.getReleaseVersion() : null;
+        }
     }
 
     /**
