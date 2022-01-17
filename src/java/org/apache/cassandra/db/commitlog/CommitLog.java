be7675939d61b066f05c9159359bf2d54f60d2a9
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-01-17 13:30:36 +0100

    STAR-1015 add new CL error policy to reject writes on sync errors (#307)
    Adds fail_writes policy to commit log failure policies, which rejects
    writes on sync errors. The policy is tested using byteman to inject
    sync errors.
    
    (cherry picked from commit 6b785ca61d3afac69d45a54579d7a3310ec621b2)

--- src/java/org/apache/cassandra/db/commitlog/CommitLog.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLog.java
@@ -21,11 +21,14 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.FileStore;
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 import java.util.zip.CRC32;
 
 import com.google.common.annotations.VisibleForTesting;
+
+import org.apache.cassandra.config.Config;
 import org.apache.cassandra.io.util.File;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -51,6 +54,7 @@ import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.apache.cassandra.utils.MBeanWrapper;
 import org.apache.cassandra.utils.MonotonicClock;
+import org.apache.cassandra.utils.NoSpamLogger;
 
 import static org.apache.cassandra.db.ColumnFamilyStore.FlushReason.STARTUP;
 import static org.apache.cassandra.db.commitlog.CommitLogSegment.Allocation;
@@ -66,6 +70,7 @@ import static org.apache.cassandra.utils.FBUtilities.updateChecksumInt;
 public class CommitLog implements CommitLogMBean
 {
     private static final Logger logger = LoggerFactory.getLogger(CommitLog.class);
+    private static final NoSpamLogger noSpamLogger = NoSpamLogger.getLogger(logger, 10, TimeUnit.SECONDS);
 
     public static final CommitLog instance = CommitLog.construct();
 
@@ -275,6 +280,19 @@ public class CommitLog implements CommitLogMBean
     }
 
     /**
+     * If there was an exception when sync-ing, and if the commit log failure policy is
+     * {@link Config.CommitFailurePolicy#fail_writes} then mutations will be rejected until
+     * the sync error is cleared, which happens after a successful sync.
+     * @return
+     */
+    @VisibleForTesting
+    public boolean shouldRejectMutations()
+    {
+        return executor.getSyncError() &&
+                DatabaseDescriptor.getCommitFailurePolicy() == Config.CommitFailurePolicy.fail_writes;
+    }
+
+    /**
      * Add a Mutation to the commit log. If CDC is enabled, this can fail.
      *
      * @param mutation the Mutation to add to the log
@@ -285,6 +303,13 @@ public class CommitLog implements CommitLogMBean
         assert mutation != null;
 
         mutation.validateSize(MessagingService.current_version, ENTRY_OVERHEAD_SIZE);
+
+        if (shouldRejectMutations())
+        {
+            String errorMsg = "Rejecting mutation due to a failure sync-ing commit log segments";
+            noSpamLogger.error(errorMsg);
+            throw new FSWriteError(new IllegalStateException(errorMsg), segmentManager.allocatingFrom().getPath());
+        }
 
         try (DataOutputBuffer dob = DataOutputBuffer.scratchBuffer.get())
         {
@@ -527,6 +552,7 @@ public class CommitLog implements CommitLogMBean
                 String errorMsg = String.format("%s. Commit disk failure policy is %s; terminating thread.", message, DatabaseDescriptor.getCommitFailurePolicy());
                 logger.error(addAdditionalInformationIfPossible(errorMsg), t);
                 return false;
+            case fail_writes:
             case ignore:
                 logger.error(addAdditionalInformationIfPossible(message), t);
                 return true;
