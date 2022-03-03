390a3691f65fff9796abed135b13fcbd114809eb
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-03-03 10:26:53 +0100

    STAR-1160 allow truncateBlocking to get endpoints set (#363)
    Refactors truncateBlocking in StorageProxy to provide set of endpoints.
    The method is not static any more, so it can be tested in Cassandra
    extensions like CNDB.
    
    Needed for CNDB and is port of fixes for CNDB-1462 and CNDB-909.

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5135,7 +5135,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
         try
         {
-            StorageProxy.truncateBlocking(keyspace, table);
+            StorageProxy.instance.truncateBlocking(keyspace, table);
         }
         catch (UnavailableException e)
         {
