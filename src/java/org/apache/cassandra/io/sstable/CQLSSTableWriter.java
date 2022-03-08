datastax/cassandra

STAR-790: Rename Schema to SchemaManager

src/java/org/apache/cassandra/io/sstable/CQLSSTableWriter.java
@@ -305,7 +305,7 @@ public class CQLSSTableWriter implements Closeable
      */
     public UserType getUDType(String dataType)
     {
-        KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(insert.keyspace());
+        KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(insert.keyspace());
         org.apache.cassandra.db.marshal.UserType userType = ksm.types.getNullable(ByteBufferUtil.bytes(dataType));
         return (UserType) UDHelper.driverType(userType);
     }
@@ -503,31 +503,31 @@ public class CQLSSTableWriter implements Closeable
 
             synchronized (CQLSSTableWriter.class)
             {
-                if (Schema.instance.getKeyspaceMetadata(SchemaConstants.SCHEMA_KEYSPACE_NAME) == null)
-                    Schema.instance.load(SchemaKeyspace.metadata());
-                if (Schema.instance.getKeyspaceMetadata(SchemaConstants.SYSTEM_KEYSPACE_NAME) == null)
-                    Schema.instance.load(SystemKeyspace.metadata());
+                if (SchemaManager.instance.getKeyspaceMetadata(SchemaConstants.SCHEMA_KEYSPACE_NAME) == null)
+                    SchemaManager.instance.load(SchemaKeyspace.metadata());
+                if (SchemaManager.instance.getKeyspaceMetadata(SchemaConstants.SYSTEM_KEYSPACE_NAME) == null)
+                    SchemaManager.instance.load(SystemKeyspace.metadata());
 
                 String keyspaceName = schemaStatement.keyspace();
 
-                if (Schema.instance.getKeyspaceMetadata(keyspaceName) == null)
+                if (SchemaManager.instance.getKeyspaceMetadata(keyspaceName) == null)
                 {
-                    Schema.instance.load(KeyspaceMetadata.create(keyspaceName,
-                                                                 KeyspaceParams.simple(1),
-                                                                 Tables.none(),
-                                                                 Views.none(),
-                                                                 Types.none(),
-                                                                 Functions.none()));
+                    SchemaManager.instance.load(KeyspaceMetadata.create(keyspaceName,
+                                                                        KeyspaceParams.simple(1),
+                                                                        Tables.none(),
+                                                                        Views.none(),
+                                                                        Types.none(),
+                                                                        Functions.none()));
                 }
 
-                KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(keyspaceName);
+                KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(keyspaceName);
 
                 TableMetadata tableMetadata = ksm.tables.getNullable(schemaStatement.table());
                 if (tableMetadata == null)
                 {
                     Types types = createTypes(keyspaceName);
                     tableMetadata = createTable(types);
-                    Schema.instance.load(ksm.withSwapped(ksm.tables.with(tableMetadata)).withSwapped(types));
+                    SchemaManager.instance.load(ksm.withSwapped(ksm.tables.with(tableMetadata)).withSwapped(types));
                 }
 
                 UpdateStatement preparedInsert = prepareInsert();

msmygit/vscassandra

Seems to be self-explanatory. No other changes made in this commit
(cherry picked from commit bf427a4f31e97afa0ced270fc4296e4f4da20883)

src/java/org/apache/cassandra/io/sstable/CQLSSTableWriter.java
@@ -306,7 +306,7 @@ public class CQLSSTableWriter implements Closeable
      */
     public UserType getUDType(String dataType)
     {
-        KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(insert.keyspace());
+        KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(insert.keyspace());
         org.apache.cassandra.db.marshal.UserType userType = ksm.types.getNullable(ByteBufferUtil.bytes(dataType));
         return (UserType) UDHelper.driverType(userType);
     }
@@ -513,31 +513,31 @@ public class CQLSSTableWriter implements Closeable
 
             synchronized (CQLSSTableWriter.class)
             {
-                if (Schema.instance.getKeyspaceMetadata(SchemaConstants.SCHEMA_KEYSPACE_NAME) == null)
-                    Schema.instance.load(Schema.getSystemKeyspaceMetadata());
-                if (Schema.instance.getKeyspaceMetadata(SchemaConstants.SYSTEM_KEYSPACE_NAME) == null)
-                    Schema.instance.load(SystemKeyspace.metadata());
+                if (SchemaManager.instance.getKeyspaceMetadata(SchemaConstants.SCHEMA_KEYSPACE_NAME) == null)
+                    SchemaManager.instance.load(SchemaManager.getSystemKeyspaceMetadata());
+                if (SchemaManager.instance.getKeyspaceMetadata(SchemaConstants.SYSTEM_KEYSPACE_NAME) == null)
+                    SchemaManager.instance.load(SystemKeyspace.metadata());
 
                 String keyspaceName = schemaStatement.keyspace();
 
-                if (Schema.instance.getKeyspaceMetadata(keyspaceName) == null)
+                if (SchemaManager.instance.getKeyspaceMetadata(keyspaceName) == null)
                 {
-                    Schema.instance.load(KeyspaceMetadata.create(keyspaceName,
-                                                                 KeyspaceParams.simple(1),
-                                                                 Tables.none(),
-                                                                 Views.none(),
-                                                                 Types.none(),
-                                                                 Functions.none()));
+                    SchemaManager.instance.load(KeyspaceMetadata.create(keyspaceName,
+                                                                        KeyspaceParams.simple(1),
+                                                                        Tables.none(),
+                                                                        Views.none(),
+                                                                        Types.none(),
+                                                                        Functions.none()));
                 }
 
-                KeyspaceMetadata ksm = Schema.instance.getKeyspaceMetadata(keyspaceName);
+                KeyspaceMetadata ksm = SchemaManager.instance.getKeyspaceMetadata(keyspaceName);
 
                 TableMetadata tableMetadata = ksm.tables.getNullable(schemaStatement.table());
                 if (tableMetadata == null)
                 {
                     Types types = createTypes(keyspaceName);
                     tableMetadata = createTable(types);
-                    Schema.instance.load(ksm.withSwapped(ksm.tables.with(tableMetadata)).withSwapped(types));
+                    SchemaManager.instance.load(ksm.withSwapped(ksm.tables.with(tableMetadata)).withSwapped(types));
                 }
 
                 UpdateStatement preparedInsert = prepareInsert();