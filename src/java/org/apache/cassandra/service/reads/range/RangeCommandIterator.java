apache/cassandra

STAR-865 Refactor and port metrics code from cndb (#247)
* STAR-865: Porting metrics from cndb-884, riptano/bdp@03b23db6a5697baaf71d46d661c0ac1c908bc33e
and riptano/bdp/#19515

Co-authored-by: Zhao Yang <jasonstack.zhao@gmail.com>
Co-authored-by: Jake Luciani <tjake@users.noreply.github.com>

* STAR-865: Porting MicrometerChunkCacheMetrics from:
CNDB-161 Add MicrometerMetrics class
CNDB-780 Add Micrometer metrics for the chunk cache

Co-authored-by: Stefania Alborghetti <stefania.alborghetti@datastax.com>

src/java/org/apache/cassandra/service/reads/range/RangeCommandIterator.java
@@ -36,6 +36,7 @@ import org.apache.cassandra.exceptions.UnavailableException;
 import org.apache.cassandra.index.Index;
 import org.apache.cassandra.locator.ReplicaPlan;
 import org.apache.cassandra.metrics.ClientRangeRequestMetrics;
+import org.apache.cassandra.metrics.CoordinatorClientRequestMetricsProvider;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.AbstractIterator;
 import org.apache.cassandra.utils.CloseableIterator;
@@ -46,7 +47,7 @@ public abstract class RangeCommandIterator extends AbstractIterator<RowIterator>
     private static final Logger logger = LoggerFactory.getLogger(RangeCommandIterator.class);
 
     @VisibleForTesting
-    public static final ClientRangeRequestMetrics rangeMetrics = new ClientRangeRequestMetrics("RangeSlice");
+    public final ClientRangeRequestMetrics rangeMetrics;
 
     protected final CloseableIterator<ReplicaPlan.ForRangeRead> replicaPlans;
     private final int totalRangeCount;
@@ -95,6 +96,7 @@ public abstract class RangeCommandIterator extends AbstractIterator<RowIterator>
                          int totalRangeCount,
                          long queryStartNanoTime)
     {
+        this.rangeMetrics = CoordinatorClientRequestMetricsProvider.instance.metrics(command.metadata().keyspace).rangeMetrics;
         this.replicaPlans = replicaPlans;
         this.command = command;
         this.concurrencyFactor = concurrencyFactor;

src/java/org/apache/cassandra/tracing/TraceStateImpl.java
@@ -35,6 +35,8 @@ import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.exceptions.OverloadedException;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.metrics.CoordinatorClientRequestMetrics;
+import org.apache.cassandra.metrics.CoordinatorClientRequestMetricsProvider;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.apache.cassandra.utils.WrappedRunnable;
@@ -118,7 +120,8 @@ public class TraceStateImpl extends TraceState
     {
         try
         {
-            StorageProxy.mutate(Collections.singletonList(mutation), ConsistencyLevel.ANY, System.nanoTime());
+            CoordinatorClientRequestMetrics metrics = CoordinatorClientRequestMetricsProvider.instance.metrics(mutation.getKeyspaceName());
+            StorageProxy.mutate(Collections.singletonList(mutation), ConsistencyLevel.ANY, System.nanoTime(), metrics);
         }
         catch (OverloadedException e)
         {

test/unit/org/apache/cassandra/metrics/CustomCoordinatorMetricsTest.java
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.metrics;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomCoordinatorMetricsTest
+{
+    @BeforeClass
+    public static void beforeClass()
+    {
+        // Sets custom client provider class used in {@code CoordinatorClientRequestMetricsProvider#instance}
+        CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY.setString(CoordinatorClientRequestMetricsProvider.DefaultCoordinatorMetricsProvider.class.getName());
+    }
+
+    @AfterClass
+    public static void teardown()
+    {
+        CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY.setString("");
+    }
+
+    @Test
+    public void testStaticInstanceWithCustomProviderClassName()
+    {
+        // Custom client provider class name set in {@link beforeClass()}
+        CoordinatorClientRequestMetricsProvider customClientRequestMetricsProvider = CoordinatorClientRequestMetricsProvider.instance;
+        assertThat(customClientRequestMetricsProvider).isInstanceOf(CoordinatorClientRequestMetricsProvider.DefaultCoordinatorMetricsProvider.class);
+        CoordinatorClientRequestMetrics metrics = customClientRequestMetricsProvider.metrics("");
+    }
+
+    @Test
+    public void testMakeProviderWithClassThatExists()
+    {
+        CoordinatorClientRequestMetricsProvider.make(CoordinatorClientRequestMetricsProvider.DefaultCoordinatorMetricsProvider.class.getName());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMakeProviderWithClassThatDoesNotExist()
+    {
+        CoordinatorClientRequestMetricsProvider.make("SomeOtherCLass");
+    }
+}

msmygit/vscassandra

(cherry picked from commit 5e0d8897b5d71a5d74a6adebe07d8060733ed0f0)
fix ConcurrencyFactorTest
the metrics require reset if we want to measure, especially the max values

src/java/org/apache/cassandra/service/reads/range/RangeCommandIterator.java
@@ -36,6 +36,7 @@ import org.apache.cassandra.exceptions.UnavailableException;
 import org.apache.cassandra.index.Index;
 import org.apache.cassandra.locator.ReplicaPlan;
 import org.apache.cassandra.metrics.ClientRangeRequestMetrics;
+import org.apache.cassandra.metrics.ClientRequestsMetricsProvider;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.AbstractIterator;
 import org.apache.cassandra.utils.CloseableIterator;
@@ -46,7 +47,7 @@ public abstract class RangeCommandIterator extends AbstractIterator<RowIterator>
     private static final Logger logger = LoggerFactory.getLogger(RangeCommandIterator.class);
 
     @VisibleForTesting
-    public static final ClientRangeRequestMetrics rangeMetrics = new ClientRangeRequestMetrics("RangeSlice");
+    public final ClientRangeRequestMetrics rangeMetrics;
 
     protected final CloseableIterator<ReplicaPlan.ForRangeRead> replicaPlans;
     private final int totalRangeCount;
@@ -95,6 +96,7 @@ public abstract class RangeCommandIterator extends AbstractIterator<RowIterator>
                          int totalRangeCount,
                          long queryStartNanoTime)
     {
+        this.rangeMetrics = ClientRequestsMetricsProvider.instance.metrics(command.metadata().keyspace).rangeMetrics;
         this.replicaPlans = replicaPlans;
         this.command = command;
         this.concurrencyFactor = concurrencyFactor;

src/java/org/apache/cassandra/tracing/TraceStateImpl.java
@@ -35,6 +35,8 @@ import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.exceptions.OverloadedException;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.metrics.ClientRequestsMetrics;
+import org.apache.cassandra.metrics.ClientRequestsMetricsProvider;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.apache.cassandra.utils.WrappedRunnable;
@@ -118,7 +120,8 @@ public class TraceStateImpl extends TraceState
     {
         try
         {
-            StorageProxy.mutate(Collections.singletonList(mutation), ConsistencyLevel.ANY, System.nanoTime());
+            ClientRequestsMetrics metrics = ClientRequestsMetricsProvider.instance.metrics(mutation.getKeyspaceName());
+            StorageProxy.mutate(Collections.singletonList(mutation), ConsistencyLevel.ANY, System.nanoTime(), metrics);
         }
         catch (OverloadedException e)
         {

test/unit/org/apache/cassandra/metrics/CustomCoordinatorMetricsTest.java
@@ -1,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.metrics;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CustomCoordinatorMetricsTest
+{
+    @BeforeClass
+    public static void beforeClass()
+    {
+        // Sets custom client provider class used in {@code ClientRequestsMetricsHolderProvider#instance}
+        CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY.setString(ClientRequestsMetricsProvider.DefaultClientRequestsMetricsProvider.class.getName());
+    }
+
+    @AfterClass
+    public static void teardown()
+    {
+        CUSTOM_CLIENT_REQUEST_METRICS_PROVIDER_PROPERTY.setString("");
+    }
+
+    @Test
+    public void testStaticInstanceWithCustomProviderClassName()
+    {
+        // Custom client provider class name set in {@link beforeClass()}
+        ClientRequestsMetricsProvider customClientRequestMetricsProvider = ClientRequestsMetricsProvider.instance;
+        assertThat(customClientRequestMetricsProvider).isInstanceOf(ClientRequestsMetricsProvider.DefaultClientRequestsMetricsProvider.class);
+        ClientRequestsMetrics metrics = customClientRequestMetricsProvider.metrics("");
+    }
+
+    @Test
+    public void testMakeProviderWithClassThatExists()
+    {
+        ClientRequestsMetricsProvider.make(ClientRequestsMetricsProvider.DefaultClientRequestsMetricsProvider.class.getName());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testMakeProviderWithClassThatDoesNotExist()
+    {
+        ClientRequestsMetricsProvider.make("SomeOtherCLass");
+    }
+}