apache/cassandra

CNDB 1707: Port remote file system abstractions to Stargazer (#273)
* LogTransaction: add ILogTransactionsFactory to provide custom log
transaction

* UCS: Port CNDB-2134 to disable shards on UCS L0

* UCS: add CompactionAggregatePrioritizer to prioritize sstables based on remote file cache

* NativeLibrary: Add INativeLibrary interface to provide custom implementation

* SSTableWatcher: to discover custom component before opening sstables

* StorageProvider: support custom file system and change Descriptor to use URI

* StorageFeatureFlags: disable features that are not supported by custom file system

* StorageHandler: to reload sstable from custom file system

src/java/org/apache/cassandra/db/lifecycle/LogFile.java
@@ -51,7 +51,9 @@ import static org.apache.cassandra.utils.Throwables.merge;
  * of unfinished leftovers when a transaction is completed, or aborted, or when
  * we clean up on start-up.
  *
- * @see LogTransaction
+ * Note: this is used by {@link LogTransaction}
+ *
+ * @see AbstractLogTransaction
  */
 final class LogFile implements AutoCloseable
 {
@@ -323,8 +325,7 @@ final class LogFile implements AutoCloseable
         for (SSTableReader sstable : tables)
         {
             File directory = sstable.descriptor.directory;
-            String fileName = StringUtils.join(directory, File.pathSeparator(), getFileName());
-            replicas.maybeCreateReplica(directory, fileName, records);
+            replicas.maybeCreateReplica(directory, getFileName(), records);
         }
         return LogRecord.make(type, tables);
     }
@@ -332,8 +333,7 @@ final class LogFile implements AutoCloseable
     private LogRecord makeAddRecord(SSTable table)
     {
         File directory = table.descriptor.directory;
-        String fileName = StringUtils.join(directory, File.pathSeparator(), getFileName());
-        replicas.maybeCreateReplica(directory, fileName, records);
+        replicas.maybeCreateReplica(directory, getFileName(), records);
         return LogRecord.make(Type.ADD, table);
     }
 
@@ -347,8 +347,7 @@ final class LogFile implements AutoCloseable
         assert type == Type.ADD || type == Type.REMOVE;
 
         File directory = table.descriptor.directory;
-        String fileName = StringUtils.join(directory, File.pathSeparator(), getFileName());
-        replicas.maybeCreateReplica(directory, fileName, records);
+        replicas.maybeCreateReplica(directory, getFileName(), records);
         return record.asType(type);
     }
 
@@ -487,7 +486,7 @@ final class LogFile implements AutoCloseable
     }
 
     @VisibleForTesting
-    List<String> getFilePaths()
+    List<File> getFilePaths()
     {
         return replicas.getFilePaths();
     }

msmygit/vscassandra

(cherry picked from commit e27ee69bb404ba4c21b65d2aef157ac54425f2ef)

src/java/org/apache/cassandra/db/lifecycle/LogFile.java
@@ -51,7 +51,9 @@ import static org.apache.cassandra.utils.Throwables.merge;
  * of unfinished leftovers when a transaction is completed, or aborted, or when
  * we clean up on start-up.
  *
- * @see LogTransaction
+ * Note: this is used by {@link LogTransaction}
+ *
+ * @see AbstractLogTransaction
  */
 final class LogFile implements AutoCloseable
 {
@@ -347,8 +349,7 @@ final class LogFile implements AutoCloseable
     private void maybeCreateReplica(SSTable sstable)
     {
         File directory = sstable.descriptor.directory;
-        String fileName = StringUtils.join(directory, File.pathSeparator(), getFileName());
-        replicas.maybeCreateReplica(directory, fileName, onDiskRecords);
+        replicas.maybeCreateReplica(directory, getFileName(), onDiskRecords);
     }
 
     void addRecord(LogRecord record)
@@ -487,7 +488,7 @@ final class LogFile implements AutoCloseable
     }
 
     @VisibleForTesting
-    List<String> getFilePaths()
+    List<File> getFilePaths()
     {
         return replicas.getFilePaths();
     }