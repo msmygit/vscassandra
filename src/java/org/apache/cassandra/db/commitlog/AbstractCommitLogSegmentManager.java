83d2a7b4958d4f6a3a9b291e66f8813ba8d1a506
Jakub Żytka <jakub.zytka@datastax.com>
2022-03-24 17:34:47 +0100

    STAR-1183: clean handling of no active segment in commitlog (#382)

--- src/java/org/apache/cassandra/db/commitlog/AbstractCommitLogSegmentManager.java
+++ src/java/org/apache/cassandra/db/commitlog/AbstractCommitLogSegmentManager.java
@@ -546,7 +546,7 @@ public abstract class AbstractCommitLogSegmentManager
      */
     CommitLogPosition getCurrentPosition()
     {
-        return allocatingFrom.getCurrentCommitLogPosition();
+        return allocatingFrom != null ? allocatingFrom.getCurrentCommitLogPosition() : CommitLogPosition.NONE;
     }
 
     /**
