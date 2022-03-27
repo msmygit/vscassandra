apache/cassandra

CNDB 1707: Port remote file system abstractions to Stargazer (#273)
* LogTransaction: add ILogTransactionsFactory to provide custom log
transaction

* UCS: Port CNDB-2134 to disable shards on UCS L0

* UCS: add CompactionAggregatePrioritizer to prioritize sstables based on remote file cache

* NativeLibrary: Add INativeLibrary interface to provide custom implementation

* SSTableWatcher: to discover custom component before opening sstables

* StorageProvider: support custom file system and change Descriptor to use URI

* StorageFeatureFlags: disable features that are not supported by custom file system

* StorageHandler: to reload sstable from custom file system

src/java/org/apache/cassandra/io/sstable/StorageHandler.java
@@ -1,0 +1,148 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.io.sstable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+
+import com.google.common.base.Preconditions;
+
+import org.apache.cassandra.db.Directories;
+import org.apache.cassandra.db.lifecycle.Tracker;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.io.sstable.format.SSTableReader;
+import org.apache.cassandra.schema.SchemaManager;
+import org.apache.cassandra.schema.TableMetadataRef;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.REMOTE_STORAGE_HANDLER;
+
+/**
+ * The handler of the storage of sstables, and possibly other files such as txn logs.
+ * <p/>
+ * If sstables are stored on the local disk, then this handler is a thin wrapper of {@link Directories.SSTableLister},
+ * but for sstables stored remotely, for example on S3, then the handler may need to perform more
+ * work, such as selecting only part of the remote sstables available, or adding new ones when offline compaction
+ * has run. This behaviour can be implemented in a sub-class that can be set with {@link #remoteStorageHandler}.
+ * <p/>
+ * Local sytem tables will always use the default storage handler, {@link DefaultStorageHandler}
+ * whereas user tables will use {@link #remoteStorageHandler} if one is set, or the default handler if none
+ * has been set.
+ * <p/>
+ */
+public abstract class StorageHandler
+{
+    private final static String remoteStorageHandler = REMOTE_STORAGE_HANDLER.getString();
+
+    public enum ReloadReason
+    {
+        /** New nodes joined or left */
+        TOPOLOGY_CHANGED,
+        /** Data was truncated */
+        TRUNCATION,
+        /** Ordinary periodic reload */
+        PERIODIC,
+        /** Data was replayed either from the commit log or a batch log */
+        DATA_REPLAYED,
+        /** When repair task started */
+        REPAIR,
+        /** A request over forced by users to reload. */
+        USER_REQUESTED,
+    }
+
+    protected final TableMetadataRef metadata;
+    protected final Directories directories;
+    protected final Tracker dataTracker;
+
+    public StorageHandler(TableMetadataRef metadata, Directories directories, Tracker dataTracker)
+    {
+        Preconditions.checkNotNull(directories, "Directories should not be null");
+
+        this.metadata = metadata;
+        this.directories = directories;
+        this.dataTracker = dataTracker;
+    }
+
+    /**
+     * @return true if the node is ready to serve data for this table. This means that the
+     *         node is not bootstrapping and that no data may be missing, e.g. if sstables are
+     *         being downloaded from remote storage or streamed from other nodes then isReady()
+     *         would return false. Generally, user read queries should not succeed if this method
+     *         returns false.
+     */
+    public abstract boolean isReady();
+
+    /**
+     * Load the initial sstables into the tracker that was passed in to the constructor.
+     *
+     * @return the sstables that were loaded
+     */
+    public abstract Collection<SSTableReader> loadInitialSSTables();
+
+    /**
+     * Reload any sstables that may have been created and not yet loaded. This is normally
+     * a no-op for the default local storage, but for remote storage implementations it
+     * signals that sstables need to be refreshed.
+     *
+     * @return the sstables that were loaded
+     */
+    public abstract Collection<SSTableReader> reloadSSTables(ReloadReason reason);
+
+    /**
+     * This method determines if the backing storage handles is remote storage
+     * <p/>
+     * @return true if storage handler is remote
+     */
+    public abstract boolean isRemote();
+
+    /**
+     * This method will run the operation specified by the {@link Runnable} passed it
+     * whilst guaranteeing the guarantees that no sstable will be loaded or unloaded
+     * whilst this operation is running, by waiting for in-progress operation to complete.
+     * In other words, the storage handler must not change the status of the tracker,
+     * or try to load any sstable as long as this operation is executing.
+     *
+     * @param runnable the operation to execute.
+     */
+    public abstract void runWithReloadingDisabled(Runnable runnable);
+
+    /**
+     * Called when the CFS is unloaded, this needs to perform any cleanup.
+     */
+    public abstract void unload();
+
+    public static StorageHandler create(TableMetadataRef metadata, Directories directories, Tracker dataTracker)
+    {
+        // local keyspaces are always local so don't use the remote handler even if it has been configured
+        if (remoteStorageHandler == null || SchemaManager.isKeyspaceWithLocalStrategy(metadata.keyspace))
+            return new DefaultStorageHandler(metadata, directories, dataTracker);
+
+        Class<StorageHandler> factoryClass =  FBUtilities.classForName(remoteStorageHandler, "Remote storage handler");
+
+        try
+        {
+            return factoryClass.getConstructor(TableMetadataRef.class, Directories.class, Tracker.class)
+                    .newInstance(metadata, directories, dataTracker);
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e)
+        {
+            throw new ConfigurationException("Unable to find correct constructor for " + remoteStorageHandler, e);
+        }
+    }
+}

src/java/org/apache/cassandra/io/storage/StorageProvider.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.io.storage;
+
+import javax.annotation.Nullable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.db.Directories;
+import org.apache.cassandra.io.sstable.Component;
+import org.apache.cassandra.io.util.File;
+import org.apache.cassandra.io.util.PathUtils;
+import org.apache.cassandra.schema.KeyspaceMetadata;
+import org.apache.cassandra.schema.SchemaManager;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.CUSTOM_STORAGE_PROVIDER;
+
+/**
+ * The storage provider is used to support directory creation and remote/local conversion for remote storage.
+ * The default implementation {@link DefaultProvider} is based on local file system.
+ */
+public interface StorageProvider
+{
+    Logger logger = LoggerFactory.getLogger(StorageProvider.class);
+
+    StorageProvider instance = !CUSTOM_STORAGE_PROVIDER.isPresent()
+        ? new DefaultProvider()
+        : FBUtilities.construct(CUSTOM_STORAGE_PROVIDER.getString(), "storage provider");
+
+    enum DirectoryType
+    {
+        DATA("data_file_directories"),
+        LOCAL_SYSTEM_DATA("local_system_data_file_directories"),
+        COMMITLOG("commit_log_directory"),
+        HINTS("hints_directory"),
+        SAVED_CACHES("saved_caches_directory"),
+        CDC("cdc_raw_directory"),
+        SNAPSHOT("snapshot_directory"),
+        NODES("nodes_local_directory"),
+        LOG_TRANSACTION("log_transaction_directory"),
+        LOGS("logs_directory"),
+        TEMP("temp_directory"),
+        OTHERS("other_directory");
+
+        final String name;
+
+        final boolean readable;
+        final boolean writable;
+
+        DirectoryType(String name)
+        {
+            this.name = name;
+            this.readable = true;
+            this.writable = true;
+        }
+    }
+
+    /**
+     * @return local path if given path is remote path, otherwise returns itself
+     */
+    File getLocalPath(File path);
+
+    /**
+     * update the given path with open options for the sstable components
+     */
+    File withOpenOptions(File ret, Component component);
+
+    /**
+     * Create data directories for given table
+     *
+     * @param ksMetadata The keyspace metadata, can be null. This is used when schema metadata is
+     *                   not available in {@link SchemaManager}, eg. CNDB backup & restore
+     * @param keyspaceName the name of the keyspace
+     * @param dirs current local data directories
+     * @return data directories that are created
+     */
+    Directories.DataDirectory[] createDataDirectories(@Nullable KeyspaceMetadata ksMetadata, String keyspaceName, Directories.DataDirectory[] dirs);
+
+    /**
+     * Create directory for the given path and type, either locally or remotely if any remote storage parameters are passed in.
+     *
+     * @param dir the directory absolute path to create
+     * @param type the type of directory to create
+     * @return the actual directory path, which can be either local or remote; or null if directory can't be created
+     */
+    File createDirectory(String dir, DirectoryType type);
+
+    class DefaultProvider implements StorageProvider
+    {
+        @Override
+        public File getLocalPath(File path)
+        {
+            return path;
+        }
+
+        @Override
+        public File withOpenOptions(File ret, Component component)
+        {
+            return ret;
+        }
+
+        @Override
+        public Directories.DataDirectory[] createDataDirectories(@Nullable KeyspaceMetadata ksMetadata, String keyspaceName, Directories.DataDirectory[] dirs)
+        {
+            // data directories are already created in DatabadeDescriptor#createAllDirectories
+            return dirs;
+        }
+
+        @Override
+        public File createDirectory(String dir, DirectoryType type)
+        {
+            File ret = new File(dir);
+            PathUtils.createDirectoriesIfNotExists(ret.toPath());
+            return ret;
+        }
+    }
+}

msmygit/vscassandra

(cherry picked from commit e27ee69bb404ba4c21b65d2aef157ac54425f2ef)

src/java/org/apache/cassandra/io/sstable/StorageHandler.java
@@ -1,0 +1,148 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.io.sstable;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
+
+import com.google.common.base.Preconditions;
+
+import org.apache.cassandra.db.Directories;
+import org.apache.cassandra.db.lifecycle.Tracker;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.io.sstable.format.SSTableReader;
+import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.schema.TableMetadataRef;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.REMOTE_STORAGE_HANDLER;
+
+/**
+ * The handler of the storage of sstables, and possibly other files such as txn logs.
+ * <p/>
+ * If sstables are stored on the local disk, then this handler is a thin wrapper of {@link Directories.SSTableLister},
+ * but for sstables stored remotely, for example on S3, then the handler may need to perform more
+ * work, such as selecting only part of the remote sstables available, or adding new ones when offline compaction
+ * has run. This behaviour can be implemented in a sub-class that can be set with {@link #remoteStorageHandler}.
+ * <p/>
+ * Local sytem tables will always use the default storage handler, {@link DefaultStorageHandler}
+ * whereas user tables will use {@link #remoteStorageHandler} if one is set, or the default handler if none
+ * has been set.
+ * <p/>
+ */
+public abstract class StorageHandler
+{
+    private final static String remoteStorageHandler = REMOTE_STORAGE_HANDLER.getString();
+
+    public enum ReloadReason
+    {
+        /** New nodes joined or left */
+        TOPOLOGY_CHANGED,
+        /** Data was truncated */
+        TRUNCATION,
+        /** Ordinary periodic reload */
+        PERIODIC,
+        /** Data was replayed either from the commit log or a batch log */
+        DATA_REPLAYED,
+        /** When repair task started */
+        REPAIR,
+        /** A request over forced by users to reload. */
+        USER_REQUESTED,
+    }
+
+    protected final TableMetadataRef metadata;
+    protected final Directories directories;
+    protected final Tracker dataTracker;
+
+    public StorageHandler(TableMetadataRef metadata, Directories directories, Tracker dataTracker)
+    {
+        Preconditions.checkNotNull(directories, "Directories should not be null");
+
+        this.metadata = metadata;
+        this.directories = directories;
+        this.dataTracker = dataTracker;
+    }
+
+    /**
+     * @return true if the node is ready to serve data for this table. This means that the
+     *         node is not bootstrapping and that no data may be missing, e.g. if sstables are
+     *         being downloaded from remote storage or streamed from other nodes then isReady()
+     *         would return false. Generally, user read queries should not succeed if this method
+     *         returns false.
+     */
+    public abstract boolean isReady();
+
+    /**
+     * Load the initial sstables into the tracker that was passed in to the constructor.
+     *
+     * @return the sstables that were loaded
+     */
+    public abstract Collection<SSTableReader> loadInitialSSTables();
+
+    /**
+     * Reload any sstables that may have been created and not yet loaded. This is normally
+     * a no-op for the default local storage, but for remote storage implementations it
+     * signals that sstables need to be refreshed.
+     *
+     * @return the sstables that were loaded
+     */
+    public abstract Collection<SSTableReader> reloadSSTables(ReloadReason reason);
+
+    /**
+     * This method determines if the backing storage handles is remote storage
+     * <p/>
+     * @return true if storage handler is remote
+     */
+    public abstract boolean isRemote();
+
+    /**
+     * This method will run the operation specified by the {@link Runnable} passed it
+     * whilst guaranteeing the guarantees that no sstable will be loaded or unloaded
+     * whilst this operation is running, by waiting for in-progress operation to complete.
+     * In other words, the storage handler must not change the status of the tracker,
+     * or try to load any sstable as long as this operation is executing.
+     *
+     * @param runnable the operation to execute.
+     */
+    public abstract void runWithReloadingDisabled(Runnable runnable);
+
+    /**
+     * Called when the CFS is unloaded, this needs to perform any cleanup.
+     */
+    public abstract void unload();
+
+    public static StorageHandler create(TableMetadataRef metadata, Directories directories, Tracker dataTracker)
+    {
+        // local keyspaces are always local so don't use the remote handler even if it has been configured
+        if (remoteStorageHandler == null || Schema.isKeyspaceWithLocalStrategy(metadata.keyspace))
+            return new DefaultStorageHandler(metadata, directories, dataTracker);
+
+        Class<StorageHandler> factoryClass =  FBUtilities.classForName(remoteStorageHandler, "Remote storage handler");
+
+        try
+        {
+            return factoryClass.getConstructor(TableMetadataRef.class, Directories.class, Tracker.class)
+                    .newInstance(metadata, directories, dataTracker);
+        }
+        catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e)
+        {
+            throw new ConfigurationException("Unable to find correct constructor for " + remoteStorageHandler, e);
+        }
+    }
+}

src/java/org/apache/cassandra/io/storage/StorageProvider.java
@@ -1,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.io.storage;
+
+import javax.annotation.Nullable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.db.Directories;
+import org.apache.cassandra.io.sstable.Component;
+import org.apache.cassandra.io.util.File;
+import org.apache.cassandra.io.util.PathUtils;
+import org.apache.cassandra.schema.KeyspaceMetadata;
+import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.config.CassandraRelevantProperties.CUSTOM_STORAGE_PROVIDER;
+
+/**
+ * The storage provider is used to support directory creation and remote/local conversion for remote storage.
+ * The default implementation {@link DefaultProvider} is based on local file system.
+ */
+public interface StorageProvider
+{
+    Logger logger = LoggerFactory.getLogger(StorageProvider.class);
+
+    StorageProvider instance = !CUSTOM_STORAGE_PROVIDER.isPresent()
+                               ? new DefaultProvider()
+                               : FBUtilities.construct(CUSTOM_STORAGE_PROVIDER.getString(), "storage provider");
+
+    enum DirectoryType
+    {
+        DATA("data_file_directories"),
+        LOCAL_SYSTEM_DATA("local_system_data_file_directories"),
+        COMMITLOG("commit_log_directory"),
+        HINTS("hints_directory"),
+        SAVED_CACHES("saved_caches_directory"),
+        CDC("cdc_raw_directory"),
+        SNAPSHOT("snapshot_directory"),
+        NODES("nodes_local_directory"),
+        LOG_TRANSACTION("log_transaction_directory"),
+        LOGS("logs_directory"),
+        TEMP("temp_directory"),
+        OTHERS("other_directory");
+
+        final String name;
+
+        final boolean readable;
+        final boolean writable;
+
+        DirectoryType(String name)
+        {
+            this.name = name;
+            this.readable = true;
+            this.writable = true;
+        }
+    }
+
+    /**
+     * @return local path if given path is remote path, otherwise returns itself
+     */
+    File getLocalPath(File path);
+
+    /**
+     * update the given path with open options for the sstable components
+     */
+    File withOpenOptions(File ret, Component component);
+
+    /**
+     * Create data directories for given table
+     *
+     * @param ksMetadata   The keyspace metadata, can be null. This is used when schema metadata is
+     *                     not available in {@link Schema}, eg. CNDB backup & restore
+     * @param keyspaceName the name of the keyspace
+     * @param dirs         current local data directories
+     * @return data directories that are created
+     */
+    Directories.DataDirectory[] createDataDirectories(@Nullable KeyspaceMetadata ksMetadata, String keyspaceName, Directories.DataDirectory[] dirs);
+
+    /**
+     * Create directory for the given path and type, either locally or remotely if any remote storage parameters are passed in.
+     *
+     * @param dir  the directory absolute path to create
+     * @param type the type of directory to create
+     * @return the actual directory path, which can be either local or remote; or null if directory can't be created
+     */
+    File createDirectory(String dir, DirectoryType type);
+
+    class DefaultProvider implements StorageProvider
+    {
+        @Override
+        public File getLocalPath(File path)
+        {
+            return path;
+        }
+
+        @Override
+        public File withOpenOptions(File ret, Component component)
+        {
+            return ret;
+        }
+
+        @Override
+        public Directories.DataDirectory[] createDataDirectories(@Nullable KeyspaceMetadata ksMetadata, String keyspaceName, Directories.DataDirectory[] dirs)
+        {
+            // data directories are already created in DatabadeDescriptor#createAllDirectories
+            return dirs;
+        }
+
+        @Override
+        public File createDirectory(String dir, DirectoryType type)
+        {
+            File ret = new File(dir);
+            PathUtils.createDirectoriesIfNotExists(ret.toPath());
+            return ret;
+        }
+    }
+}