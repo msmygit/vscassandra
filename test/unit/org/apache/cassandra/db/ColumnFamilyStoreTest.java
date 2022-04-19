datastax/cassandra

STAR-247: TrieIndex SSTable format implementation
[ddc51ba35cd6312eb2529e48dfd659cf1f39506f] STAR-247: Add bigtable unit test configuration

[afbedc62b3cb097901d0eaf11b44900a75721f34] STAR-247: Disable potentiallyUnclosedCloseable in eclipse_compiler.properties

[fcd91fb96e42b7010874386f852cebe7e5f20c8c] STAR-247: Minor methods and fields signature changes and non invasive refactorings

[a808a209628547049076a64d889010acc14d08df] STAR-247: Move and rename BigTableZeroCopyWriter to SSTableZeroCopyWriter

[ffe6182b3bd4dad5d1e4a6bd15ee294b4a97c426] STAR-247: Don't hardcode sets of components of an SSTable
Use sets predefined per format instead

[a4fb1f14133824cdf62e582775aa02dff0eacfa2] STAR-247: Refactor creation of SSTableReader, use factory where possible
Previously SSTableReader was created usually in the following scheme:
static openXXX method -> SSTableReaderBuilder -> factory method -> constructor

Instead of that, we will use the scheme:
factory method -> static method -> builder -> constructor

The intermediate static method is left to keep the amount of changes minimal

[c27c2b0a64a822aa01e7fe6a7f8335f29545acd7] STAR-247: Update SSTable version - introduce DSE-specific components
Those components will be ignored for now, but we don't want to fail loading
such SSTables.

[f8efff91bd3108230b0b39855a6861eddbf384e9] STAR-247: Change contract of SSTR.keyAt and SSTFO.startPartition
The previous contract states that those methods accept a key position
in the primary index file. We need to change the contract as
primary index is not an obvious component as BTI format does not
have it. Instead, those methods will use a key position in data file.

[d7a2268265ec939ad97d446f8223beafad88d6cc] STAR-247: Refactor usages of default file handle builder
In particular, index file handle builder will take a component as an argument

[ce68cb7fe3f90b15ce040ee776fabf25cc93e05f] STAR-247: Fix in Walker (add to STAR-75)

[17ad86e983f7f44054eec2c8b6f2d4d15c5ec92f] STAR-247: Use RowInde

test/unit/org/apache/cassandra/db/ColumnFamilyStoreTest.java
@@ -264,12 +264,11 @@ public class ColumnFamilyStoreTest
         new RowUpdateBuilder(cfs.metadata(), 0, ByteBufferUtil.bytes("key2")).clustering("Column1").add("val", "asdf").build().applyUnsafe();
         cfs.forceBlockingFlush(ColumnFamilyStore.FlushReason.UNIT_TESTS);
 
-        for (int version = 1; version <= 2; ++version)
+        for (SSTableReader liveSSTable : cfs.getLiveSSTables())
         {
-            Descriptor existing = new Descriptor(cfs.getDirectories().getDirectoryForNewSSTables(), KEYSPACE2, CF_STANDARD1, version,
-                                                 SSTableFormat.Type.BIG);
-            Descriptor desc = new Descriptor(Directories.getBackupsDirectory(existing), KEYSPACE2, CF_STANDARD1, version, SSTableFormat.Type.BIG);
-            for (Component c : new Component[]{ Component.DATA, Component.PRIMARY_INDEX, Component.FILTER, Component.STATS })
+            Descriptor existing = liveSSTable.descriptor;
+            Descriptor desc = new Descriptor(Directories.getBackupsDirectory(existing), KEYSPACE2, CF_STANDARD1, liveSSTable.descriptor.generation, liveSSTable.descriptor.formatType);
+            for (Component c : liveSSTable.components)
                 assertTrue("Cannot find backed-up file:" + desc.filenameFor(c), new File(desc.filenameFor(c)).exists());
         }
     }

msmygit/vscassandra

test/unit/org/apache/cassandra/db/ColumnFamilyStoreTest.java
@@ -269,12 +269,8 @@ public class ColumnFamilyStoreTest
         for (SSTableReader liveSSTable : cfs.getLiveSSTables())
         {
             Descriptor existing = liveSSTable.descriptor;
-            Descriptor desc = new Descriptor(Directories.getBackupsDirectory(existing),
-                                             KEYSPACE2,
-                                             CF_STANDARD1,
-                                             liveSSTable.descriptor.id,
-                                             liveSSTable.descriptor.formatType);
-            for (Component c : liveSSTable.getComponents())
+            Descriptor desc = new Descriptor(Directories.getBackupsDirectory(existing), KEYSPACE2, CF_STANDARD1, liveSSTable.descriptor.id, liveSSTable.descriptor.formatType);
+            for (Component c : liveSSTable.components)
                 assertTrue("Cannot find backed-up file:" + desc.filenameFor(c), new File(desc.filenameFor(c)).exists());
         }
     }