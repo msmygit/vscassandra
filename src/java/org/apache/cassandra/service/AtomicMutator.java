apache/cassandra

STAR-985: allow substituting StorageProxy::mutateAtomically implementation (#316)
* STAR-985: allow substituting StorageProxy::mutateAtomically implementation
dse batchlog deserialization

src/java/org/apache/cassandra/service/AtomicMutator.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.service;
+
+import java.util.Collection;
+
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.Mutation;
+import org.apache.cassandra.exceptions.OverloadedException;
+import org.apache.cassandra.exceptions.UnavailableException;
+import org.apache.cassandra.exceptions.WriteTimeoutException;
+import org.apache.cassandra.metrics.CoordinatorClientRequestMetrics;
+
+public interface AtomicMutator
+{
+    void mutateAtomically(Collection<Mutation> mutations,
+            ConsistencyLevel consistencyLevel,
+            boolean requireQuorumForRemove,
+            long queryStartNanoTime,
+            CoordinatorClientRequestMetrics metrics,
+            ClientState clientState)
+            throws UnavailableException, OverloadedException, WriteTimeoutException;
+}

msmygit/vscassandra

(cherry picked from commit 285c08d30cd529a225f740b7c92dd5504cb7e4b1)

src/java/org/apache/cassandra/service/AtomicMutator.java
@@ -1,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.service;
+
+import java.util.Collection;
+
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.Mutation;
+import org.apache.cassandra.exceptions.OverloadedException;
+import org.apache.cassandra.exceptions.UnavailableException;
+import org.apache.cassandra.exceptions.WriteTimeoutException;
+import org.apache.cassandra.metrics.ClientRequestsMetrics;
+
+public interface AtomicMutator
+{
+    void mutateAtomically(Collection<Mutation> mutations,
+                          ConsistencyLevel consistencyLevel,
+                          boolean requireQuorumForRemove,
+                          long queryStartNanoTime,
+                          ClientRequestsMetrics metrics,
+                          ClientState clientState)
+    throws UnavailableException, OverloadedException, WriteTimeoutException;
+}