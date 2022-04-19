mahmudulhasancsedu5/cassandra-ucs

STAR-508: Switch Trie to Cursor representation
This replaces the Node-based walks and transformations. The result is
drastically less intermediate object creation, improved performance
and somewhat simpler code at the expense of the concept being a little
harder to understand initially.

Adds further documentation and expands tests for sliced tries.

src/java/org/apache/cassandra/db/tries/SingletonTrie.java
@@ -22,7 +22,6 @@ import org.apache.cassandra.utils.bytecomparable.ByteSource;
 
 /**
  * Singleton trie, mapping the given key to value.
- * Formed as a chain of single-child SNodes leading to one ENode with no children and the given value as content.
  */
 class SingletonTrie<T> extends Trie<T>
 {
@@ -35,87 +34,69 @@ class SingletonTrie<T> extends Trie<T>
         this.value = value;
     }
 
-    private class ENode<L> extends NoChildrenNode<T, L>
+    public Cursor cursor()
     {
-        ENode(L parent)
-        {
-            super(parent);
-        }
-
-        @Override
-        public T content()
-        {
-            return value;
-        }
+        return new Cursor();
     }
 
-    private class SNode<L> extends Node<T, L>
+    class Cursor implements Trie.Cursor<T>
     {
-        private final ByteSource source;
-        boolean requested = false;
+        ByteSource src = key.asComparableBytes(BYTE_COMPARABLE_VERSION);
+        int currentDepth = 0;
+        int currentTransition = -1;
+        int nextTransition = src.next();
 
-        SNode(int trans, L parent, ByteSource source)
+        public int advance()
         {
-            super(parent);
-            this.currentTransition = trans;
-            this.source = source;
-        }
-
-        @Override
-        public Node<T, L> getCurrentChild(L parent)
-        {
-            // Requesting more than once will screw up the iteration of source.
-            assert !requested : "getCurrentChild can only be called once for a given transition.";
-            requested = true;
-            return makeNode(parent, source);
-        }
-
-        @Override
-        public Node<T, L> getUniqueDescendant(L parentLink, TransitionsReceiver receiver)
-        {
-            if (receiver != null)
+            currentTransition = nextTransition;
+            if (currentTransition != ByteSource.END_OF_STREAM)
             {
-                receiver.add(currentTransition);
-                int next;
-                while ((next = source.next()) != ByteSource.END_OF_STREAM)
-                {
-                    receiver.add(next);
-                }
+                nextTransition = src.next();
+                return ++currentDepth;
             }
-
-            return new ENode<>(parentLink);
+            else
+                return currentDepth = -1;
         }
 
         @Override
-        public Remaining startIteration()
+        public int advanceMultiple(TransitionsReceiver receiver)
         {
-            return Remaining.ONE;
+            if (nextTransition == ByteSource.END_OF_STREAM)
+                return currentDepth = -1;
+            int current = nextTransition;
+            int depth = currentDepth;
+            int next = src.next();
+            while (next != ByteSource.END_OF_STREAM)
+            {
+                if (receiver != null)
+                    receiver.add(current);
+                current = next;
+                next = src.next();
+                ++depth;
+            }
+            currentTransition = current;
+            nextTransition = next;
+            return currentDepth = ++depth;
         }
 
-        @Override
-        public Remaining advanceIteration()
+        public int skipChildren()
         {
-            return null;
+            return currentDepth = -1;  // no alternatives
         }
 
-        @Override
+        public int depth()
+        {
+            return currentDepth;
+        }
+
         public T content()
         {
-            return null;
+            return nextTransition == ByteSource.END_OF_STREAM ? value : null;
         }
-    }
 
-    private <L> Node<T, L> makeNode(L parent, ByteSource source)
-    {
-        int next = source.next();
-        if (next == ByteSource.END_OF_STREAM)
-            return new ENode<>(parent);
-        else
-            return new SNode<>(next, parent, source);
-    }
-
-    public <L> Node<T, L> root()
-    {
-        return makeNode(null, key.asComparableBytes(BYTE_COMPARABLE_VERSION));
+        public int incomingTransition()
+        {
+            return currentTransition;
+        }
     }
 }

src/java/org/apache/cassandra/db/tries/TriePathReconstructor.java
@@ -1,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.db.tries;
+
+import java.util.Arrays;
+
+import org.agrona.concurrent.UnsafeBuffer;
+import org.apache.cassandra.utils.bytecomparable.ByteComparable;
+
+public class TriePathReconstructor implements Trie.ResettingTransitionsReceiver
+{
+    protected byte[] keyBytes = new byte[32];
+    protected int keyPos = 0;
+
+    public void add(int t)
+    {
+        if (keyPos >= keyBytes.length)
+            keyBytes = Arrays.copyOf(keyBytes, keyPos * 2);
+        keyBytes[keyPos++] = (byte) t;
+    }
+
+    public void add(UnsafeBuffer b, int pos, int count)
+    {
+        int newPos = keyPos + count;
+        if (newPos > keyBytes.length)
+            keyBytes = Arrays.copyOf(keyBytes, Math.max(newPos + 16, keyBytes.length * 2));
+        b.getBytes(pos, keyBytes, keyPos, count);
+        keyPos = newPos;
+    }
+
+    public void reset(int newLength)
+    {
+        keyPos = newLength;
+    }
+
+    static ByteComparable toByteComparable(byte[] bytes, int byteLength)
+    {
+        return ByteComparable.fixedLength(Arrays.copyOf(bytes, byteLength));
+    }
+}

msmygit/vscassandra

(cherry picked from commit 2b3c4c50d65a7ae93f0ee9bc9848064ff03733d2)

src/java/org/apache/cassandra/db/tries/SingletonTrie.java
@@ -22,7 +22,6 @@ import org.apache.cassandra.utils.bytecomparable.ByteSource;
 
 /**
  * Singleton trie, mapping the given key to value.
- * Formed as a chain of single-child SNodes leading to one ENode with no children and the given value as content.
  */
 class SingletonTrie<T> extends Trie<T>
 {
@@ -35,87 +34,69 @@ class SingletonTrie<T> extends Trie<T>
         this.value = value;
     }
 
-    private class ENode<L> extends NoChildrenNode<T, L>
+    public Cursor cursor()
     {
-        ENode(L parent)
-        {
-            super(parent);
-        }
-
-        @Override
-        public T content()
-        {
-            return value;
-        }
+        return new Cursor();
     }
 
-    private class SNode<L> extends Node<T, L>
+    class Cursor implements Trie.Cursor<T>
     {
-        private final ByteSource source;
-        boolean requested = false;
+        ByteSource src = key.asComparableBytes(BYTE_COMPARABLE_VERSION);
+        int currentDepth = 0;
+        int currentTransition = -1;
+        int nextTransition = src.next();
 
-        SNode(int trans, L parent, ByteSource source)
+        public int advance()
         {
-            super(parent);
-            this.currentTransition = trans;
-            this.source = source;
-        }
-
-        @Override
-        public Node<T, L> getCurrentChild(L parent)
-        {
-            // Requesting more than once will screw up the iteration of source.
-            assert !requested : "getCurrentChild can only be called once for a given transition.";
-            requested = true;
-            return makeNode(parent, source);
-        }
-
-        @Override
-        public Node<T, L> getUniqueDescendant(L parentLink, TransitionsReceiver receiver)
-        {
-            if (receiver != null)
+            currentTransition = nextTransition;
+            if (currentTransition != ByteSource.END_OF_STREAM)
             {
-                receiver.add(currentTransition);
-                int next;
-                while ((next = source.next()) != ByteSource.END_OF_STREAM)
-                {
-                    receiver.add(next);
-                }
+                nextTransition = src.next();
+                return ++currentDepth;
             }
-
-            return new ENode<>(parentLink);
+            else
+                return currentDepth = -1;
         }
 
         @Override
-        public Remaining startIteration()
+        public int advanceMultiple(TransitionsReceiver receiver)
         {
-            return Remaining.ONE;
+            if (nextTransition == ByteSource.END_OF_STREAM)
+                return currentDepth = -1;
+            int current = nextTransition;
+            int depth = currentDepth;
+            int next = src.next();
+            while (next != ByteSource.END_OF_STREAM)
+            {
+                if (receiver != null)
+                    receiver.addPathByte(current);
+                current = next;
+                next = src.next();
+                ++depth;
+            }
+            currentTransition = current;
+            nextTransition = next;
+            return currentDepth = ++depth;
         }
 
-        @Override
-        public Remaining advanceIteration()
+        public int skipChildren()
         {
-            return null;
+            return currentDepth = -1;  // no alternatives
         }
 
-        @Override
+        public int depth()
+        {
+            return currentDepth;
+        }
+
         public T content()
         {
-            return null;
+            return nextTransition == ByteSource.END_OF_STREAM ? value : null;
         }
-    }
 
-    private <L> Node<T, L> makeNode(L parent, ByteSource source)
-    {
-        int next = source.next();
-        if (next == ByteSource.END_OF_STREAM)
-            return new ENode<>(parent);
-        else
-            return new SNode<>(next, parent, source);
-    }
-
-    public <L> Node<T, L> root()
-    {
-        return makeNode(null, key.asComparableBytes(BYTE_COMPARABLE_VERSION));
+        public int incomingTransition()
+        {
+            return currentTransition;
+        }
     }
 }

src/java/org/apache/cassandra/db/tries/TriePathReconstructor.java
@@ -1,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.db.tries;
+
+import java.util.Arrays;
+
+import org.agrona.concurrent.UnsafeBuffer;
+import org.apache.cassandra.utils.bytecomparable.ByteComparable;
+
+public class TriePathReconstructor implements Trie.ResettingTransitionsReceiver
+{
+    protected byte[] keyBytes = new byte[32];
+    protected int keyPos = 0;
+
+    public void addPathByte(int nextByte)
+    {
+        if (keyPos >= keyBytes.length)
+            keyBytes = Arrays.copyOf(keyBytes, keyPos * 2);
+        keyBytes[keyPos++] = (byte) nextByte;
+    }
+
+    public void addPathBytes(UnsafeBuffer buffer, int pos, int count)
+    {
+        int newPos = keyPos + count;
+        if (newPos > keyBytes.length)
+            keyBytes = Arrays.copyOf(keyBytes, Math.max(newPos + 16, keyBytes.length * 2));
+        buffer.getBytes(pos, keyBytes, keyPos, count);
+        keyPos = newPos;
+    }
+
+    public void resetPathLength(int newLength)
+    {
+        keyPos = newLength;
+    }
+
+    static ByteComparable toByteComparable(byte[] bytes, int byteLength)
+    {
+        return ByteComparable.fixedLength(Arrays.copyOf(bytes, byteLength));
+    }
+}