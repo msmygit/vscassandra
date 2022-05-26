mahmudulhasancsedu5/cassandra-ucs

STAR-791: Introduce SSTable and CFS abstractions for compaction
The main objective of this refactoring is to enable compaction strategies
to operate on a lean abstraction of an sstable and the compaction space
instead of the full-blown open SSTableReader and ColumnFamilyStore. The
compaction process itself must still operate on SSTableReaders which
provide the mechanisms for reading the data; switching between the two
representations is done when compaction signals it is ready to start
compaction on a set of sstables via the realm's tryModify method.

Most files in the compaction package have been changed to rely solely on
CompactionSSTable and CompactionRealm, with the exception of
CompactionManager and BackgroundCompactionsRunner, which are part of the
CFS implementation.

Also does some small fixes and simplifications identified during the
refactoring:

- Fixes bloom filter size in Upgrader calculated for splitting to
  compaction strategy's sstable size limit while files weren't actually
  split.
- Stops checking an sstable's bloom filter if its minTimestamp is already
  above the current min for purge functions.
- Some collection construction/processing simplifications.
- Breaks up compaction -> CFS -> compaction reference cycles.
- Refactors some method to lower their complexity as requested by sonarcloud.
- Changes some remaining ...LatencyPerKb names to ...TimePerKb.

src/java/org/apache/cassandra/db/compaction/CompactionLogger.java
@@ -171,25 +171,25 @@ public class CompactionLogger
     private JsonNode formatSSTables(CompactionStrategy strategy)
     {
         ArrayNode node = json.arrayNode();
-        for (SSTableReader sstable : strategy.getSSTables())
+        for (CompactionSSTable sstable : strategy.getSSTables())
             node.add(formatSSTable(sstable));
 
         return node;
     }
 
-    private JsonNode formatSSTable(SSTableReader sstable)
+    private JsonNode formatSSTable(CompactionSSTable sstable)
     {
         ObjectNode node = json.objectNode();
-        node.put("generation", sstable.descriptor.generation.asString());
-        node.put("version", sstable.descriptor.version.getVersion());
+        node.put("generation", sstable.getDescriptor().generation.asString());
+        node.put("version", sstable.getDescriptor().version.getVersion());
         node.put("size", sstable.onDiskLength());
 
         // The details are only relevant or available for some strategies, e.g. LCS or Date tiered but
         // it doesn't hurt to log them all the time in order to simplify things
         ObjectNode details = json.objectNode();
         details.put("level", sstable.getSSTableLevel());
-        details.put("min_token", sstable.first.getToken().toString());
-        details.put("max_token", sstable.last.getToken().toString());
+        details.put("min_token", sstable.getFirst().getToken().toString());
+        details.put("max_token", sstable.getLast().getToken().toString());
         details.put("min_timestamp", sstable.getMinTimestamp());
         details.put("max_timestamp", sstable.getMaxTimestamp());
 

msmygit/vscassandra

(cherry picked from commit 943ae99a14950006406b2f77f314e23027f200fe)
(cherry picked from commit e0fd645ab0edf2d35adf88cfb6fd15beaf4432e9)

src/java/org/apache/cassandra/db/compaction/CompactionLogger.java
@@ -171,25 +171,25 @@ public class CompactionLogger
     private JsonNode formatSSTables(CompactionStrategy strategy)
     {
         ArrayNode node = json.arrayNode();
-        for (SSTableReader sstable : strategy.getSSTables())
+        for (CompactionSSTable sstable : strategy.getSSTables())
             node.add(formatSSTable(sstable));
 
         return node;
     }
 
-    private JsonNode formatSSTable(SSTableReader sstable)
+    private JsonNode formatSSTable(CompactionSSTable sstable)
     {
         ObjectNode node = json.objectNode();
-        node.put("generation", sstable.descriptor.id.toString());
-        node.put("version", sstable.descriptor.version.getVersion());
+        node.put("generation", sstable.getDescriptor().id.toString());
+        node.put("version", sstable.getDescriptor().version.getVersion());
         node.put("size", sstable.onDiskLength());
 
         // The details are only relevant or available for some strategies, e.g. LCS or Date tiered but
         // it doesn't hurt to log them all the time in order to simplify things
         ObjectNode details = json.objectNode();
         details.put("level", sstable.getSSTableLevel());
-        details.put("min_token", sstable.first.getToken().toString());
-        details.put("max_token", sstable.last.getToken().toString());
+        details.put("min_token", sstable.getFirst().getToken().toString());
+        details.put("max_token", sstable.getLast().getToken().toString());
         details.put("min_timestamp", sstable.getMinTimestamp());
         details.put("max_timestamp", sstable.getMaxTimestamp());
 