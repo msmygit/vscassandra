datastax/cassandra

STAR-996: Implement node information component
The stores all the information in memory guaranteeing proper behaviour in case of concurrent access.
The changes are serialized and stored in system tables (as it was so far).

src/java/org/apache/cassandra/hints/HintsDispatchTrigger.java
@@ -19,7 +19,8 @@ package org.apache.cassandra.hints;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import org.apache.cassandra.gms.Gossiper;
+import org.apache.cassandra.nodes.NodeInfo;
+import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.schema.SchemaManager;
 
 /**
@@ -62,7 +63,7 @@ final class HintsDispatchTrigger implements Runnable
                .filter(store -> !isScheduled(store))
                .filter(HintsStore::isLive)
                .filter(store -> store.isWriting() || store.hasFiles())
-               .filter(store -> SchemaManager.instance.isSameVersion(Gossiper.instance.getSchemaVersion(store.address())))
+               .filter(store -> SchemaManager.instance.isSameVersion(Nodes.localOrPeerInfoOpt(store.address()).map(NodeInfo::getSchemaVersion).orElse(null)))
                .forEach(this::schedule);
     }
 

msmygit/vscassandra

(cherry picked from commit db0d3ccd22c72c534ccba8c8d244e2422aa3595a)
(cherry picked from commit b1dc27ded8b644d461756ee53e53f384f72a7a9d)

src/java/org/apache/cassandra/hints/HintsDispatchTrigger.java
@@ -20,7 +20,8 @@ package org.apache.cassandra.hints;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.apache.cassandra.gms.ApplicationState;
-import org.apache.cassandra.gms.Gossiper;
+import org.apache.cassandra.nodes.NodeInfo;
+import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.schema.Schema;
 
 import static org.apache.cassandra.utils.FBUtilities.getBroadcastAddressAndPort;
@@ -65,7 +66,7 @@ final class HintsDispatchTrigger implements Runnable
                .filter(store -> !isScheduled(store))
                .filter(HintsStore::isLive)
                .filter(store -> store.isWriting() || store.hasFiles())
-               .filter(store -> Schema.instance.isSameVersion(Gossiper.instance.getSchemaVersion(store.address())))
+               .filter(store -> Schema.instance.isSameVersion(Nodes.localOrPeerInfoOpt(store.address()).map(NodeInfo::getSchemaVersion).orElse(null)))
                .forEach(this::schedule);
     }
 