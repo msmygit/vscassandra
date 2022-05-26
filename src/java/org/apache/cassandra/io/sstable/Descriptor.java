datastax/cassandra

CNDB-1707: Port remote file system abstractions to Stargazer (#273)
* LogTransaction: add ILogTransactionsFactory to provide custom log
transaction

* UCS: Port CNDB-2134 to disable shards on UCS L0

* UCS: add CompactionAggregatePrioritizer to prioritize sstables based on remote file cache

* NativeLibrary: Add INativeLibrary interface to provide custom implementation

* SSTableWatcher: to discover custom component before opening sstables

* StorageProvider: support custom file system and change Descriptor to use URI

* StorageFeatureFlags: disable features that are not supported by custom file system

* StorageHandler: to reload sstable from custom file system

(cherry picked from commit e27ee69bb404ba4c21b65d2aef157ac54425f2ef)

src/java/org/apache/cassandra/io/sstable/Descriptor.java
@@ -30,6 +30,7 @@ import org.apache.cassandra.io.sstable.format.SSTableFormat;
 import org.apache.cassandra.io.sstable.format.Version;
 import org.apache.cassandra.io.sstable.metadata.IMetadataSerializer;
 import org.apache.cassandra.io.sstable.metadata.MetadataSerializer;
+import org.apache.cassandra.io.storage.StorageProvider;
 import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.utils.Pair;
 import org.apache.cassandra.utils.UUIDGen;
@@ -61,6 +62,9 @@ public class Descriptor
     public final SSTableUniqueIdentifier generation;
     public final SSTableFormat.Type formatType;
     private final int hashCode;
+
+    private final String baseFileURI;
+    private final String filenamePart;
 
     /**
      * A descriptor that assumes CURRENT_VERSION.
@@ -96,6 +100,12 @@ public class Descriptor
         this.formatType = formatType;
 
         hashCode = Objects.hashCode(version, this.directory, generation, ksname, cfname, formatType);
+
+        filenamePart = version.toString() + separator + generation + separator + formatType.name;
+        String locationURI = directory.toUri().toString();
+        if (!locationURI.endsWith(java.io.File.separator))
+            locationURI = locationURI + java.io.File.separatorChar;
+        baseFileURI = locationURI + filenamePart;
     }
 
     public Descriptor withGeneration(SSTableUniqueIdentifier newGeneration)
@@ -110,12 +120,8 @@ public class Descriptor
 
     public File tmpFileFor(Component component)
     {
-        return new File(tmpFilenameFor(component));
-    }
-
-    public String tmpFilenameFor(Component component)
-    {
-        return filenameFor(component) + TMP_EXT;
+        File file = StorageProvider.instance.getLocalPath(fileFor(component));
+        return file.resolveSibling(file.name() + TMP_EXT);
     }
 
     /**
@@ -125,32 +131,17 @@ public class Descriptor
     {
         // Use UUID to handle concurrent streamings on the same sstable.
         // TMP_EXT allows temp file to be removed by {@link ColumnFamilyStore#scrubDataDirectories}
-        return String.format("%s.%s%s", filenameFor(component), UUIDGen.getTimeUUID(), TMP_EXT);
+        return String.format("%s.%s%s", fileFor(component), UUIDGen.getTimeUUID(), TMP_EXT);
     }
 
     public File fileFor(Component component)
     {
-        return new File(filenameFor(component));
+        return component.getFile(baseFileURI);
     }
 
-    public String filenameFor(Component component)
+    public String baseFileUri()
     {
-        return baseFilename() + separator + component.name();
-    }
-
-    public String baseFilename()
-    {
-        StringBuilder buff = new StringBuilder();
-        buff.append(directory).append(File.pathSeparator());
-        appendFileName(buff);
-        return buff.toString();
-    }
-
-    private void appendFileName(StringBuilder buff)
-    {
-        buff.append(version).append(separator);
-        buff.append(generation);
-        buff.append(separator).append(formatType.name);
+        return baseFileURI;
     }
 
     public String relativeFilenameFor(Component component)
@@ -161,9 +152,7 @@ public class Descriptor
             buff.append(directory.name()).append(File.pathSeparator());
         }
 
-        appendFileName(buff);
-        buff.append(separator).append(component.name());
-        return buff.toString();
+        return buff.append(filenamePart).append(separator).append(component.name()).toString();
     }
 
     public SSTableFormat getFormat()
@@ -347,7 +336,7 @@ public class Descriptor
     @Override
     public String toString()
     {
-        return baseFilename();
+        return baseFileUri();
     }
 
     @Override

test/unit/org/apache/cassandra/io/sstable/SSTableUtils.java
@@ -92,7 +92,7 @@ public class SSTableUtils
         File cfDir = new File(tempdir, keyspaceName + File.pathSeparator() + cfname);
         cfDir.tryCreateDirectories();
         cfDir.deleteOnExit();
-        File datafile = new File(new Descriptor(cfDir, keyspaceName, cfname, generation, SSTableFormat.Type.BIG).filenameFor(Component.DATA));
+        File datafile = new Descriptor(cfDir, keyspaceName, cfname, generation, SSTableFormat.Type.BIG).fileFor(Component.DATA);
         if (!datafile.createFileIfNotExists())
             throw new IOException("unable to create file " + datafile);
         datafile.deleteOnExit();
@@ -228,7 +228,7 @@ public class SSTableUtils
 
         public Collection<SSTableReader> write(int expectedSize, Appender appender) throws IOException
         {
-            File datafile = (dest == null) ? tempSSTableFile(ksname, cfname, generation) : new File(dest.filenameFor(Component.DATA));
+            File datafile = (dest == null) ? tempSSTableFile(ksname, cfname, generation) : dest.fileFor(Component.DATA);
             TableMetadata metadata = Schema.instance.getTableMetadata(ksname, cfname);
             ColumnFamilyStore cfs = Schema.instance.getColumnFamilyStoreInstance(metadata.id);
             SerializationHeader header = appender.header();
@@ -240,7 +240,7 @@ public class SSTableUtils
             if (cleanup)
                 for (SSTableReader reader: readers)
                     for (Component component : reader.components)
-                        new File(reader.descriptor.filenameFor(component)).deleteOnExit();
+                        reader.descriptor.fileFor(component).deleteOnExit();
             return readers;
         }
     }

msmygit/vscassandra

(cherry picked from commit e98d05a294ad2d62d1b46dad081b5f60a16b7064)

src/java/org/apache/cassandra/io/sstable/Descriptor.java
@@ -30,6 +30,7 @@ import org.apache.cassandra.io.sstable.format.SSTableFormat;
 import org.apache.cassandra.io.sstable.format.Version;
 import org.apache.cassandra.io.sstable.metadata.IMetadataSerializer;
 import org.apache.cassandra.io.sstable.metadata.MetadataSerializer;
+import org.apache.cassandra.io.storage.StorageProvider;
 import org.apache.cassandra.io.util.File;
 import org.apache.cassandra.utils.Pair;
 import org.apache.cassandra.utils.UUIDGen;
@@ -63,6 +64,9 @@ public class Descriptor
     public final SSTableId id;
     public final SSTableFormat.Type formatType;
     private final int hashCode;
+
+    private final String baseFileURI;
+    private final String filenamePart;
 
     /**
      * A descriptor that assumes CURRENT_VERSION.
@@ -98,6 +102,12 @@ public class Descriptor
         this.formatType = formatType;
 
         hashCode = Objects.hashCode(version, this.directory, id, ksname, cfname, formatType);
+
+        filenamePart = version.toString() + separator + id + separator + formatType.name;
+        String locationURI = directory.toUri().toString();
+        if (!locationURI.endsWith(java.io.File.separator))
+            locationURI = locationURI + java.io.File.separatorChar;
+        baseFileURI = locationURI + filenamePart;
     }
 
     public Descriptor withGeneration(SSTableId newId)
@@ -112,12 +122,8 @@ public class Descriptor
 
     public File tmpFileFor(Component component)
     {
-        return new File(tmpFilenameFor(component));
-    }
-
-    public String tmpFilenameFor(Component component)
-    {
-        return filenameFor(component) + TMP_EXT;
+        File file = StorageProvider.instance.getLocalPath(fileFor(component));
+        return file.resolveSibling(file.name() + TMP_EXT);
     }
 
     /**
@@ -127,7 +133,7 @@ public class Descriptor
     {
         // Use UUID to handle concurrent streamings on the same sstable.
         // TMP_EXT allows temp file to be removed by {@link ColumnFamilyStore#scrubDataDirectories}
-        return String.format("%s.%s%s", filenameFor(component), UUIDGen.getTimeUUID(), TMP_EXT);
+        return String.format("%s.%s%s", fileFor(component), UUIDGen.getTimeUUID(), TMP_EXT);
     }
 
     public String filenameFor(Component component)
@@ -137,7 +143,7 @@ public class Descriptor
 
     public File fileFor(Component component)
     {
-        return new File(filenameFor(component));
+        return component.getFile(baseFileURI);
     }
 
     public String baseFilename()
@@ -155,6 +161,11 @@ public class Descriptor
         buff.append(separator).append(formatType.name);
     }
 
+    public String baseFileUri()
+    {
+        return baseFileURI;
+    }
+
     public String relativeFilenameFor(Component component)
     {
         final StringBuilder buff = new StringBuilder();
@@ -163,9 +174,7 @@ public class Descriptor
             buff.append(directory.name()).append(File.pathSeparator());
         }
 
-        appendFileName(buff);
-        buff.append(separator).append(component.name());
-        return buff.toString();
+        return buff.append(filenamePart).append(separator).append(component.name()).toString();
     }
 
     public SSTableFormat getFormat()
@@ -349,7 +358,7 @@ public class Descriptor
     @Override
     public String toString()
     {
-        return baseFilename();
+        return baseFileUri();
     }
 
     @Override

test/unit/org/apache/cassandra/io/sstable/SSTableUtils.java
@@ -92,7 +92,7 @@ public class SSTableUtils
         File cfDir = new File(tempdir, keyspaceName + File.pathSeparator() + cfname);
         cfDir.tryCreateDirectories();
         cfDir.deleteOnExit();
-        File datafile = new File(new Descriptor(cfDir, keyspaceName, cfname, id, SSTableFormat.Type.BIG).filenameFor(Component.DATA));
+        File datafile = new Descriptor(cfDir, keyspaceName, cfname, id, SSTableFormat.Type.BIG).fileFor(Component.DATA);
         if (!datafile.createFileIfNotExists())
             throw new IOException("unable to create file " + datafile);
         datafile.deleteOnExit();
@@ -228,7 +228,7 @@ public class SSTableUtils
 
         public Collection<SSTableReader> write(int expectedSize, Appender appender) throws IOException
         {
-            File datafile = (dest == null) ? tempSSTableFile(ksname, cfname, id) : new File(dest.filenameFor(Component.DATA));
+            File datafile = (dest == null) ? tempSSTableFile(ksname, cfname, id) : dest.fileFor(Component.DATA);
             TableMetadata metadata = Schema.instance.getTableMetadata(ksname, cfname);
             ColumnFamilyStore cfs = Schema.instance.getColumnFamilyStoreInstance(metadata.id);
             SerializationHeader header = appender.header();
@@ -240,7 +240,7 @@ public class SSTableUtils
             if (cleanup)
                 for (SSTableReader reader: readers)
                     for (Component component : reader.components)
-                        new File(reader.descriptor.filenameFor(component)).deleteOnExit();
+                        reader.descriptor.fileFor(component).deleteOnExit();
             return readers;
         }
     }