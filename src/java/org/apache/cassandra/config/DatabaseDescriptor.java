6982fb5750d70cff47097d4ee13316294175cd6f
Mike Adamson <madamson@datastax.com>
2022-07-04 10:02:50 +0100

    Debug fixes

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1608,7 +1608,7 @@ public class DatabaseDescriptor
 
     public static int getNumTokens()
     {
-        return conf.num_tokens == null ? -1 : conf.num_tokens;
+        return conf.num_tokens;
     }
 
     public static InetAddressAndPort getReplaceAddress()
