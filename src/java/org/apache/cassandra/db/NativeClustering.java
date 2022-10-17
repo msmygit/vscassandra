datastax/cassandra

STAR-767: Do not copy to heap for flushing
This was failing because off-heap native clustering keys were
used in stats metadata without being copied, referencing memory
that could be overwritten.

Also fixes a problem creating retainable/minimized versions of
clustering bounds and boundaries.

src/java/org/apache/cassandra/db/NativeClustering.java
@@ -25,6 +25,7 @@ import org.apache.cassandra.db.marshal.ByteBufferAccessor;
 import org.apache.cassandra.db.marshal.ValueAccessor;
 import org.apache.cassandra.utils.ObjectSizes;
 import org.apache.cassandra.utils.concurrent.OpOrder;
+import org.apache.cassandra.utils.memory.HeapAllocator;
 import org.apache.cassandra.utils.memory.MemoryUtil;
 import org.apache.cassandra.utils.memory.NativeAllocator;
 
@@ -35,11 +36,6 @@ public class NativeClustering implements Clustering<ByteBuffer>
     private final long peer;
 
     private NativeClustering() { peer = 0; }
-
-    public ClusteringPrefix<ByteBuffer> minimize()
-    {
-        return this;
-    }
 
     public NativeClustering(NativeAllocator allocator, OpOrder.Group writeOp, Clustering<?> clustering)
     {
@@ -156,5 +152,20 @@ public class NativeClustering implements Clustering<ByteBuffer>
     public final boolean equals(Object o)
     {
         return ClusteringPrefix.equals(this, o);
+    }
+
+    public ClusteringPrefix<ByteBuffer> retainable()
+    {
+        assert kind() == Kind.CLUSTERING; // tombstones are never stored natively
+
+        // always extract
+        ByteBuffer[] values = new ByteBuffer[size()];
+        for (int i = 0; i < values.length; ++i)
+        {
+            ByteBuffer value = get(i);
+            values[i] = value != null ? HeapAllocator.instance.clone(value) : null;
+        }
+
+        return accessor().factory().clustering(values);
     }
 }

src/java/org/apache/cassandra/utils/memory/SlabAllocator.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.concurrent.OpOrder;
+import org.assertj.core.util.VisibleForTesting;
 
 /**
 + * The SlabAllocator is a bump-the-pointer allocator that allocates
@@ -152,7 +153,8 @@ public class SlabAllocator extends MemtableBufferAllocator
         }
     }
 
-    protected AbstractAllocator allocator(OpOrder.Group writeOp)
+    @VisibleForTesting
+    public AbstractAllocator allocator(OpOrder.Group writeOp)
     {
         return new ContextAllocator(writeOp, this);
     }

msmygit/vscassandra

(cherry picked from commit f00e340447f0be54cbb6ccb72a5b31baf94a14d7)
(cherry picked from commit f0904a323f7370d7cded6654b16e12273cdf923f)
(cherry picked from commit 80a0383d1d26b5676194fda63daddf285e44a631)
(cherry picked from commit a7354ef5fb529541e9a16859b2ee62544af79733)

src/java/org/apache/cassandra/db/NativeClustering.java
@@ -25,6 +25,7 @@ import org.apache.cassandra.db.marshal.ByteBufferAccessor;
 import org.apache.cassandra.db.marshal.ValueAccessor;
 import org.apache.cassandra.utils.ObjectSizes;
 import org.apache.cassandra.utils.concurrent.OpOrder;
+import org.apache.cassandra.utils.memory.HeapCloner;
 import org.apache.cassandra.utils.memory.MemoryUtil;
 import org.apache.cassandra.utils.memory.NativeAllocator;
 
@@ -35,11 +36,6 @@ public class NativeClustering implements Clustering<ByteBuffer>
     private final long peer;
 
     private NativeClustering() { peer = 0; }
-
-    public ClusteringPrefix<ByteBuffer> minimize()
-    {
-        return this;
-    }
 
     public NativeClustering(NativeAllocator allocator, OpOrder.Group writeOp, Clustering<?> clustering)
     {
@@ -156,5 +152,20 @@ public class NativeClustering implements Clustering<ByteBuffer>
     public final boolean equals(Object o)
     {
         return ClusteringPrefix.equals(this, o);
+    }
+
+    public ClusteringPrefix<ByteBuffer> retainable()
+    {
+        assert kind() == Kind.CLUSTERING; // tombstones are never stored natively
+
+        // always extract
+        ByteBuffer[] values = new ByteBuffer[size()];
+        for (int i = 0; i < values.length; ++i)
+        {
+            ByteBuffer value = get(i);
+            values[i] = value != null ? HeapCloner.instance.clone(value) : null;
+        }
+
+        return accessor().factory().clustering(values);
     }
 }

src/java/org/apache/cassandra/utils/memory/SlabAllocator.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.concurrent.OpOrder;
+import org.assertj.core.util.VisibleForTesting;
 
 /**
 + * The SlabAllocator is a bump-the-pointer allocator that allocates
@@ -152,9 +153,10 @@ public class SlabAllocator extends MemtableBufferAllocator
         }
     }
 
-    public Cloner cloner(OpOrder.Group writeOp)
+    @Override
+    public Cloner cloner(OpOrder.Group opGroup)
     {
-        return allocator(writeOp);
+        return allocator(opGroup);
     }
 
     /**