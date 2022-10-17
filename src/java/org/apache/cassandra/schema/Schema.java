apache/cassandra

STAR-1482 Log message fixes for SingleCompactorTest (#513)
* STAR-1482: Partial port of CNDB-946

Port debug logs from DSE that were added in CNDB-946 to help diagnose
slowness when dropping a table. These are needed because the
SingleCompactorTest CNDB integration test expects to find some of these
log messages in the log files.

I skipped the log messages for the schema code since it's very different
between C* and DSE and because different log messages already exist.
Plus, some log statements have already been added as part of other CC
porting work.

* STAR-1482: Sync compaction controller strings with DSE

CNDB's SingleCompactorTest integration test expects specific strings to
be in the logs. Make the CC logs match DSE so that tests pass but also
because they provide more info for users anyway.

src/java/org/apache/cassandra/schema/Schema.java
@@ -736,6 +736,7 @@ public class Schema implements SchemaProvider
             if (ks == null)
                 return;
 
+            logger.debug("Dropping keyspace {}", keyspace.name);
             keyspace.views.forEach(v -> dropView(ks, v, dropData));
             keyspace.tables.forEach(t -> dropTable(ks, t, dropData));
 
@@ -747,6 +748,7 @@ public class Schema implements SchemaProvider
 
         if (initialized)
         {
+            logger.debug("Awaiting on write barrier before dropping keyspace {}", keyspace.name);
             Keyspace.writeOrder.awaitNewBarrier();
         }
 

msmygit/vscassandra

(cherry picked from commit 8b3f11684629a33d597b35537d56dbdff8f24cbd)

src/java/org/apache/cassandra/schema/Schema.java
@@ -752,6 +752,7 @@ public class Schema implements SchemaProvider
             if (keyspace == null)
                 return;
 
+            logger.debug("Dropping keyspace {}", keyspaceMetadata.name);
             keyspaceMetadata.views.forEach(v -> dropView(keyspace, v, dropData));
             keyspaceMetadata.tables.forEach(t -> dropTable(keyspace, t, dropData));
 
@@ -761,6 +762,7 @@ public class Schema implements SchemaProvider
                 unload(keyspaceMetadata);
             });
             assert unloadedKeyspace == keyspace;
+            logger.debug("Awaiting on write barrier before dropping keyspace {}", keyspaceMetadata.name);
             Keyspace.writeOrder.awaitNewBarrier();
         }
         else