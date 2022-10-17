apache/cassandra

Implement sstable generation identifier as uuid
Patch by Jacek Lewandowski, reviewed by Dan Jatniecks, Benjamin Lerer, Andrés de la Peña for CASSANDRA-17048

src/java/org/apache/cassandra/config/Config.java
@@ -748,6 +748,9 @@ public class Config
     public volatile boolean auto_optimise_full_repair_streams = false;
     public volatile boolean auto_optimise_preview_repair_streams = false;
 
+    // see CASSANDRA-17048 and the comment in cassandra.yaml
+    public boolean enable_uuid_generation_identifiers = false;
+
     /**
      * Client mode means that the process is a pure client, that uses C* code base but does
      * not read or write local C* database files.

msmygit/vscassandra

Patch by Jacek Lewandowski; reviewed by Andrés de la Peña, Benjamin Lerer and Dan Jatnieks for CASSANDRA-17048
(cherry picked from commit e4689148628408d2c3237d5dff71bb01d5077041)
(cherry picked from commit 6fb7ef85d02a63f8932fcabf149f82e8c8207851)

src/java/org/apache/cassandra/config/Config.java
@@ -566,6 +566,9 @@ public class Config
     public volatile boolean auto_optimise_full_repair_streams = false;
     public volatile boolean auto_optimise_preview_repair_streams = false;
 
+    // see CASSANDRA-17048 and the comment in cassandra.yaml
+    public boolean enable_uuid_sstable_identifiers = false;
+
     /**
      * Client mode means that the process is a pure client, that uses C* code base but does
      * not read or write local C* database files.