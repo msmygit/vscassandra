apache/cassandra

STAR-1329 Implement storage persistence and virtual tables for Nodes (#445)
STAR-1329: Use on-disk storage for Nodes

Replaces the Nodes implementation with the implementation from 6.8 and replaces local, peers and peers_v2 tables with virtual tables

src/java/org/apache/cassandra/net/OutboundConnectionSettings.java
@@ -32,6 +32,7 @@ import org.apache.cassandra.config.EncryptionOptions.ServerEncryptionOptions;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.locator.IEndpointSnitch;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.utils.FBUtilities;
 
 import static org.apache.cassandra.config.DatabaseDescriptor.getEndpointSnitch;
@@ -459,7 +460,7 @@ public class OutboundConnectionSettings
     {
         InetAddressAndPort connectTo = this.connectTo;
         if (connectTo == null)
-            connectTo = SystemKeyspace.getPreferredIP(to);
+            connectTo = Nodes.peers().getPreferred(to);
         return connectTo;
     }
 

msmygit/vscassandra

(cherry picked from commit 46da723e4bbe09efc622ec3502ad6dfd28cfe615)
(cherry picked from commit 01e3c1b3f30366a3682b3d04d76a2b93cedcfe22)

src/java/org/apache/cassandra/net/OutboundConnectionSettings.java
@@ -32,6 +32,7 @@ import org.apache.cassandra.config.EncryptionOptions.ServerEncryptionOptions;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.locator.IEndpointSnitch;
 import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.nodes.Nodes;
 import org.apache.cassandra.utils.FBUtilities;
 
 import static org.apache.cassandra.config.DatabaseDescriptor.getEndpointSnitch;
@@ -459,7 +460,7 @@ public class OutboundConnectionSettings
     {
         InetAddressAndPort connectTo = this.connectTo;
         if (connectTo == null)
-            connectTo = SystemKeyspace.getPreferredIP(to);
+            connectTo = Nodes.peers().getPreferredIP(to);
         if (FBUtilities.getBroadcastAddressAndPort().equals(connectTo))
             return FBUtilities.getLocalAddressAndPort();
         return connectTo;