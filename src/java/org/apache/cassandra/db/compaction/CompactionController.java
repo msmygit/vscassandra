83fbf4cf2aa70058be2d15ac9faee74ae55575f4
Ethan Brown <ethan.brown@datastax.com>
2023-08-18 13:02:15 -0700

    Overlaps should be refreshed regardless if ignoreOverlaps is activated

--- src/java/org/apache/cassandra/db/compaction/CompactionController.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionController.java
@@ -93,12 +93,6 @@ public class CompactionController extends AbstractCompactionController
             return;
         }
 
-        if (ignoreOverlaps())
-        {
-            logger.debug("not refreshing overlaps - running with ignoreOverlaps activated");
-            return;
-        }
-
         if (cfs.getNeverPurgeTombstones())
         {
             logger.debug("not refreshing overlaps for {}.{} - neverPurgeTombstones is enabled", cfs.keyspace.getName(), cfs.getTableName());
@@ -123,7 +117,7 @@ public class CompactionController extends AbstractCompactionController
         if (this.overlappingSSTables != null)
             close();
 
-        if (compacting == null || ignoreOverlaps())
+        if (compacting == null)
             overlappingSSTables = Refs.tryRef(Collections.<SSTableReader>emptyList());
         else
             overlappingSSTables = cfs.getAndReferenceOverlappingLiveSSTables(compacting);
