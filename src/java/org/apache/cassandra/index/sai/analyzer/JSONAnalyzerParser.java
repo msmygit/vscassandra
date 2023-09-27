b4df893aae9839a30b8a2a58200d502ad8b8f7f1
Michael Marshall <mmarshall@apache.org>
2023-09-27 11:16:44 -0500

    Override lucene's default for min/max gram size on ngram tokenizer

--- src/java/org/apache/cassandra/index/sai/analyzer/JSONAnalyzerParser.java
+++ src/java/org/apache/cassandra/index/sai/analyzer/JSONAnalyzerParser.java
@@ -19,6 +19,7 @@
 package org.apache.cassandra.index.sai.analyzer;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
@@ -28,6 +29,8 @@ import org.apache.cassandra.exceptions.InvalidRequestException;
 import org.apache.cassandra.index.sai.analyzer.filter.BuiltInAnalyzers;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
+import org.apache.lucene.analysis.ngram.NGramFilterFactory;
+import org.apache.lucene.analysis.ngram.NGramTokenizerFactory;
 
 public class JSONAnalyzerParser
 {
@@ -65,7 +68,9 @@ public class JSONAnalyzerParser
         }
         else
         {
-            builder.withTokenizer(analyzerModel.getTokenizer().getName(), analyzerModel.getTokenizer().getArgs());
+            String name = analyzerModel.getTokenizer().getName();
+            Map<String, String> args = analyzerModel.getTokenizer().getArgs();
+            builder.withTokenizer(name, applyTokenizerDefaults(name, args));
         }
         for (LuceneClassNameAndArgs filter : analyzerModel.getFilters())
         {
@@ -98,5 +103,16 @@ public class JSONAnalyzerParser
             }
         }
         return null;
+    }
+
+    private static Map<String, String> applyTokenizerDefaults(String filterName, Map<String, String> args)
+    {
+        if (NGramTokenizerFactory.NAME.equalsIgnoreCase(filterName))
+        {
+            // Lucene's defaults are 1 and 2 respectively, which has a large memory overhead.
+            args.putIfAbsent("minGramSize", "3");
+            args.putIfAbsent("maxGramSize", "7");
+        }
+        return args;
     }
 }
